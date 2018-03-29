3fbed8d9fd38a775b66ff2f56336959901e87970
b2blosangeles <b2blosangeles@gmail.com>
2018-03-28 20:00:56 -0700

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -215,7 +215,7 @@
 var allKeys = [];
 listAllKeys();
 function listAllKeys() {
-    s3.listObjectsV2(params, function (err, data) {
+    me.s3.listObjectsV2(params, function (err, data) {
         if (err) {
             console.log(err, err.stack); // an error occurred
         } else {
