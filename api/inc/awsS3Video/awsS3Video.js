5f6947fb3d4b5d0be347fb14cefd737f10597a17
b2blosangeles <b2blosangeles@gmail.com>
2018-04-01 08:46:47 -0700

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -285,7 +285,7 @@
 						for(var i=0;i < arr.length;i = i+size) res.push(arr.slice(i,i+size));
 						return res;
 					}
-					var t_arr =  F2D(diff, 2)
+					var t_arr =  F2D(diff, 5)
 					for (var t in t_arr) {
 						_f1['P_' + t] = (function(t) { 
 							return function(cbk1) {
