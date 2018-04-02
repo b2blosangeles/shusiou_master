9ec5c1ba6905f3532eb88d52032a0566681230dc
b2blosangeles <b2blosangeles@gmail.com>
2018-04-02 13:04:35 -0700

    Update awsS3VideoAdmin.js

--- api/inc/awsS3Video/awsS3VideoAdmin.js
+++ api/inc/awsS3Video/awsS3VideoAdmin.js
@@ -25,52 +25,6 @@
 		this.removeVidFromSpace = function(space, cbk) {
 			cbk(space);
 			return true;
-			let me = this,
-			    _p = video_name.match(/(.+)\/([^\/]+)$/);
-				
-			me.source_path = _p[1] + '/';
-			me.source_file = _p[2];
-			me.space_id = space.space_id;
-			me.space_url = space.space_url;
-			me.space_info = 'shusiou_' +  config.environment + '/' + me.source_file + '/_info.txt';
-			me.trunkSize = 512 * 1024;
-			me.vid = vid;
-			
-			pkg.request(me.space_url +  me.space_info, 
-				function (err, res, body) {
-					let v = (err) ? false : {};
-					if (v !== false) { 
-						try {  v = JSON.parse(body); } catch (e) { v = false; }
-					}
-					if (!v || !v.status || !v.status._s || !v.status._t) {
-						let CP_A = new pkg.crowdProcess(), 
-						    _fA = {};
-						
-						_fA['_s'] = function (cbks) { me.split('_s', video_name, cbks); }
-						_fA['_t'] = function (cbks) { me.split('_t', video_name, cbks); }
-						CP_A.parallel( _fA,
-							function(results) {
-								cbk(results);
-							},
-							50000
-						);											
-
-					} else {
-						me.doneDBVideoStatus(v, function(d) {
-							if (d) {
-								let tmp_root = '/var/shusiou_cache/tmpvideo/' + me.source_file + '/';
-								pkg.exec('rm -fr ' + tmp_root + ' && rm -fr ' + video_name, 
-									function(err, stdout, stderr) {
-										cbk('This video already been processed.' + me.vid);
-								//	me.load();
-								});								 
-							}
-						});
-
-					}
-				});
-			
-
 		}
 		this.doneDBVideoStatus = function(v, cbk) {
 			let me = this;
@@ -87,9 +41,7 @@
 			} else {
 				cbk(false);
 			}
-		}		
-		this.split = function(_type, _file, _cbk) {
-		};			
+		}				
 			
 		this.init = function() {
 			let me = this;
@@ -100,61 +52,6 @@
 			    secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
 			});
 			
-		}
-		this.getInfo = function(space_infoname, south_name, cbk) {
-			let me = this;
-			pkg.request(space_infoname, 
-			function (err, res, body) {
-				let v = (err) ? false : {};
-				if (v !== false) { 
-					try {  v = JSON.parse(body); } catch (e) { v = false; }
-				}
-				if (v === false) { 
-					let buff = new Buffer(100);
-					pkg.fs.stat(south_name, function(err, stat) {
-						if (err) {
-							cbk(false);
-							return true;
-						}
-						pkg.fs.open(south_name, 'r', function(err, fd) {
-							pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
-								if (err) {
-									CP.exit = 1;
-									cbk(err.message);
-									return true;
-								}									
-								var start = buffer.indexOf(new Buffer('mvhd')) + 17;
-								var timeScale = buffer.readUInt32BE(start, 4);
-								var duration = buffer.readUInt32BE(start + 4, 4);
-								var movieLength = Math.floor(duration/timeScale);
-								var v = {filesize:stat.size,time_scale:timeScale, trunksize: me.trunkSize,
-									duration: duration, length:movieLength, status:{}};
-								me.writeInfo(v, function() {
-									cbk(v);
-								});
-							});
-						});
-					});		
-				} else {
-					cbk(v);
-				}
-			});			
-		}
-		this.writeInfo = function(v, cbk) {
-			let me = this,
-			    params = {
-				Body: JSON.stringify(v),
-				Bucket: me.space_id,
-				Key: me.space_info,
-				ContentType: 'text/plain',
-				ACL: 'public-read'
-			};	
-
-			me.s3.putObject(params, function(err, data) {
-				if (err) cbk(false);
-				else cbk(true);
-				// me.doneDBVideoStatus(v, cbk);
-			});		
 		}
 		this.removeObjects = function(folder, list, callback) {
 			let me = this;
@@ -170,43 +67,6 @@
 				else callback(d);
 			});
 		}
-		this.splitVideo = function(_type, tmp_folder, cbk) {
-			let me = this;
-			switch(_type) {
-				case '_t':
-					pkg.exec('rm -f ' + tmp_folder + '* ' + ' && rm -f ' + tmp_folder + '*.* ' +
-						 '&& split -b ' + me.trunkSize + ' ' + me.source_path +  me.source_file +  ' ' + tmp_folder + '', 					 
-						{maxBuffer: 1024 * 500}, 
-						 function(err, stdout, stderr) {
-							if (err) {
-								cbk({err:err.message});
-							} else {
-								pkg.fs.readdir( tmp_folder, (err1, files) => {
-									cbk((err1) ? {err:err1.message} : {list:files});
-								});			
-							} 
-						});
-					break;
-				case '_s':
-					pkg.exec('rm -f ' + tmp_folder + '* ' + ' && rm -f ' + tmp_folder + '*.* ' +
-						 '&& ffmpeg -i ' + me.source_path +  me.source_file + 
-						 ' -c copy -map 0 -segment_time 5 -reset_timestamps 1 -f segment ' + tmp_folder + 's_%d.mp4', 
-						 {maxBuffer: 1024 * 500}, 
-						function(err, stdout, stderr) {
-							if (err) {
-								cbk({err:err.message});
-							} else {
-								pkg.fs.readdir( tmp_folder, (err1, files) => {
-									cbk((err1) ? {err:err1.message} : {list:files});
-								});			
-							}
-						});
-					break;	
-				default:
-					cbk({err:'Missing _type'});
-			}		
-		}		
-		
 		this.init();
 	};
 	module.exports = obj;
