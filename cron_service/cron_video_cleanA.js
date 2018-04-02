996e3bd4d079859e6d45a69cfb419e7e185c9459
b2blosangeles <b2blosangeles@gmail.com>
2018-04-02 08:20:38 -0700

    Update cron_video_cleanA.js

--- cron_service/cron_video_cleanA.js
+++ cron_service/cron_video_cleanA.js
@@ -1,41 +1,34 @@
-var path = require('path'), env = {root_path:path.join(__dirname, '../../..')};
+/* ---  This cron is to upload video to a aws standard object space.  */
 
+var path = require('path');
+var env = {root_path:path.join(__dirname, '../../..')};
 env.site_path = env.root_path + '/sites/master';
 env.config_path = '/var/qalet_config';
+
 var config = require(env.config_path + '/config.json');
 
-let mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-    crowdProcess =  require(env.root_path + '/package/crowdProcess/crowdProcess'),
-    fs = require('fs'),
-    folderP = require(env.site_path + '/api/inc/folderP/folderP'),
-    cfg0 = config.db,
-    CP = new crowdProcess(), 
-    _f = {};
+let pkg = {
+    	mysql		: require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+    	crowdProcess	: require(env.root_path + '/package/crowdProcess/crowdProcess'),
+	request		: require(env.root_path + '/package/request/node_modules/request'),
+	exec		: require('child_process').exec,
+	fs 		: require('fs')
+}; 
 
-let environment = config.environment;
+let awsS3VideoAdmin = require(env.site_path + '/api/inc/awsS3Video/awsS3VideoAdmin.js');
+let tm = new Date().getTime();
 
-console.log(environment); 
-
-_f['need_remove']  = function(cbk_s) { 
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-	var str = 'SELECT * FRom `video_space` WHERE `vid` NOT IN (SELECT `vid` FROM `video_user` WHERE 1)';
-	
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error || !results.length) {
-			cbk_s(false);
-		} else {
-			cbk_s(results);
-		}	
+function s() {
+	let delta_time0 = new Date().getTime() - tm;
+	console.log('---- load at ----> ' +  delta_time0);
+	var videoAdmin = new awsS3VideoAdmin(config, env, pkg, tm);	
+	 videoAdmin.delete(function(data) {
+		let delta_time = new Date().getTime() - tm;
+		console.log(data);
+		if (delta_time < 50000 && data !== 'No new id at all') {
+			s();
+		}		
+		
 	});
-};
-
-CP.serial(
-	_f,
-	function(data_s) {
-		console.log(JSON.stringify(data_s));
-	},
-	10000
-);
-
+}
+s();
