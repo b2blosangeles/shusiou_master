68cd2c9bac8d17defbdc4d7317e008ef29503931
b2blosangeles <b2blosangeles@gmail.com>
2018-04-02 07:13:54 -0700

    Update cron_video_cleanA.js

--- cron_service/cron_video_cleanA.js
+++ cron_service/cron_video_cleanA.js
@@ -1,24 +1,18 @@
-/* ---  This cron is to appoint node for video. add record link only */
-/* ---  chennel only related with user video, not related with video only */
-var path = require('path');
-var env = {root_path:path.join(__dirname, '../..')};
-env.site_path = env.root_path + '/site';
-env.site_path = env.root_path + '/site';
-env.config_path = '/var/qalet_config';
+var path = require('path'), env = {root_path:path.join(__dirname, '../../..')};
 
+env.site_path = env.root_path + '/sites/master';
+env.config_path = '/var/qalet_config';
 var config = require(env.config_path + '/config.json');
 
-var default_cnt = 2;
-
-var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+let mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
     crowdProcess =  require(env.root_path + '/package/crowdProcess/crowdProcess'),
+    fs = require('fs'),
+    folderP = require(env.site_path + '/api/inc/folderP/folderP'),
     cfg0 = config.db,
-    fs = require('fs');
-
-
-var CP_s = new crowdProcess();
-var _f_s = {};		
-_f_s['need_remove']  = function(cbk_s) { /* get database catched local videos */
+    CP = new crowdProcess(), 
+    _f = {};
+		
+_f['need_remove']  = function(cbk_s) { 
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 	var str = 'SELECT * FROM `video_user` WHERE 1';
@@ -33,9 +27,8 @@ _f_s['need_remove']  = function(cbk_s) { /* get database catched local videos */
 	});
 };
 
-
-CP_s.serial(
-	_f_s,
+CP.serial(
+	_f,
 	function(data_s) {
 		console.log(JSON.stringify(data_s));
 	},
