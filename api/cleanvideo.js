e0aa2e6e38afdff2e834f8ec6ddf2d504823775b
b2blosangeles <b2blosangeles@gmail.com>
2018-04-02 17:43:59 -0700

    Update cleanvideo.js

--- api/cleanvideo.js
+++ api/cleanvideo.js
@@ -1,7 +1,6 @@
 
-// pkg.mysql	=  require(env.site_path + '/api/inc/mysql/node_modules/mysql'); 
-var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-    config = require(env.config_path + '/config.json'),
+pkg.mysql	=  require(env.site_path + '/api/inc/mysql/node_modules/mysql'); 
+var config = require(env.config_path + '/config.json'),
     cfg0 = config.db;
 
 delete require.cache[env.sites_path + '/master/api/inc/awsS3Video/awsS3VideoAdmin.js'];
