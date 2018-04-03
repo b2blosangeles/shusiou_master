7881025980e5b467ebb69a37da0d9185e58a39c2
b2blosangeles <b2blosangeles@gmail.com>
2018-04-02 17:56:12 -0700

    Update awsS3VideoAdmin.js

--- api/inc/awsS3Video/awsS3VideoAdmin.js
+++ api/inc/awsS3Video/awsS3VideoAdmin.js
@@ -14,10 +14,10 @@
 			connection.query(str, function (error, results, fields) {
 				connection.end();
 				if (error || !results.length) {
-					delete_ca();llback(false);
+					delete_callback(false);
 				} else {
-					delete_callback(results[0]);
-					// me.removeVidFromSpace(results[0], delete_callback); 
+					// delete_callback(results[0]);
+					me.removeVidFromSpace(results[0], delete_callback); 
 				}	
 			});			
 			return true;
