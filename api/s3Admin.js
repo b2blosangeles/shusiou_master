2fedb558d9eb3bf62adbb9fb24063c67b5cfdb31
b2blosangeles <b2blosangeles@gmail.com>
2018-04-03 06:56:46 -0700

    Update s3Admin.js

--- api/s3Admin.js
+++ api/s3Admin.js
@@ -10,12 +10,9 @@ pkg.mysql = mysql;
 delete require.cache[env.sites_path + '/master/api/inc/awsS3Video/awsS3VideoAdmin.js'];
 let awsS3VideoAdmin = require(env.sites_path + '/master/api/inc/awsS3Video/awsS3VideoAdmin.js');
  
-let tm = new Date().getTime();
-
 
 var videoAdmin = new awsS3VideoAdmin(config, env, pkg, tm);	
- videoAdmin.delete(function(data) {
-	//let delta_time = new Date().getTime() - tm;
+ videoAdmin.getBuckets(function(data) {
 	res.send(data);
 
 });
