4cfe0ac14e04826c548f8263be54159fb33ea447
b2blosangeles <b2blosangeles@gmail.com>
2018-04-04 23:36:14 -0700

    Update awsS3VideoAdmin.js

--- api/inc/awsS3Video/awsS3VideoAdmin.js
+++ api/inc/awsS3Video/awsS3VideoAdmin.js
@@ -60,7 +60,7 @@
 				var vids = CP.data.getVids;
 				var CP1 = new pkg.crowdProcess();
 				var _f1 = {};
-				for (var j = 0; j <100; j++) {
+				for (var j = 0; j < 10; j++) {
 				for (var i = 0; i < vids.length; i++) {
 					_f1['p_' + i + (j * 10)] = (function(i) {
 						return function(cbk1) {
