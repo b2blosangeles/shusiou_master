0a228c89dd05d5a6519269986073398d7655f39f
b2blosangeles <b2blosangeles@gmail.com>
2018-04-04 22:56:00 -0700

    Update awsS3VideoAdmin.js

--- api/inc/awsS3Video/awsS3VideoAdmin.js
+++ api/inc/awsS3Video/awsS3VideoAdmin.js
@@ -7,12 +7,12 @@
 		};	
 		this.getBuckets = function(getBuckets_callback) {
 			let me = this;
-			var params = {};
 			
 			var CP = new pkg.crowdProcess();
 			var _f = {};
 			
 			_f['getVids'] = function(cbk0) {
+				var params = {};
 				me.s3.listBuckets(params, function(err, data) {
 					if(err) {
 						getBuckets_callback({err:err.message});
@@ -63,7 +63,45 @@
 				for (var i = 0; i < vids.length; i++) {
 					_f1['p_' + i] = (function(i) {
 						return function(cbk1) {
-							cbk1(vids[i]);
+							
+//-----
+							let total_size = 0, file_cnt = 0, v = [];
+							let recursive_f = function(Marker, cbk) {
+								var params1 = { 
+									Bucket: data.Buckets[0].Name,
+									Delimiter: '',
+									MaxKeys : 1000,
+									Marker : Marker,
+									Delimiter: '/',
+									Prefix: "shusiou_dev/"
+								};
+
+								me.s3.listObjects(params1, function (err, data) {
+									if(err) {
+										cbk({err:err.message});
+										return true;
+									} else {
+
+										for (var i = 0; i < data.CommonPrefixes.length; i++) {
+											v.push(data.CommonPrefixes[i].Prefix);
+											// total_size +=  data.Contents[i].Size;
+											// file_cnt ++;
+										}
+
+										if (data.IsTruncated) {
+											recursive_f(data.NextMarker, cbk)
+
+										} else {
+											cbk(v);
+											// cbk({file_cnt:file_cnt, total_size : total_size});
+										}
+									}
+								});						
+							}
+							recursive_f('',cbk1);
+//-----
+							
+							
 						}
 					})(i);
 				}
