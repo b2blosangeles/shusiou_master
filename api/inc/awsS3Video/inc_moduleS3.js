50626bfa09bfefd521825a2fc3de44013891ea53
b2blosangeles <b2blosangeles@gmail.com>
2018-04-06 13:43:04 -0700

    Update inc_moduleS3.js

--- api/inc/awsS3Video/inc_moduleS3.js
+++ api/inc/awsS3Video/inc_moduleS3.js
@@ -12,7 +12,11 @@
 					for (var i = 0; i < data.Buckets.length; i++) {
 						_f[data.Buckets[i].Name] = (function(i) {
 							return function(cbk){
-								cbk(i)
+								me.getBucketsVids(data.Buckets[i].Name,
+									function(data) {
+										cbk(data)
+									}
+								);
 							}
 						})(i)
 					}
@@ -26,52 +30,45 @@
 				}
 			});	
 		}
-		this.getBucketsVids = function(cbk) {	
+		this.getBucketsVids = function(bucket_name, cbk) {	
 			var  me = this;
 			var CP = new pkg.crowdProcess();
 			var _f = {};
-			me.getBuckets(
-				function(Buckets) {
-					getBucketsVids_callback(Buckets);
-					return true;
-					/*
-					let total_size = 0, file_cnt = 0, v = [];
-					let recursive_f = function(Marker, cbk) {
-						var params1 = { 
-							Bucket: Buckets[0].Name,
-							Delimiter: '',
-							MaxKeys : 1000,
-							Marker : Marker,
-							Delimiter: '/',
-							Prefix: "shusiou_dev/"
-						};
 
-						s3.listObjects(params1, function (err, data) {
-							if(err) {
-								getBucketsVids_callback({err:err.message});
-								return true;
-							} else {
+			let total_size = 0, file_cnt = 0, v = [];
+			let recursive_f = function(Marker, cbk) {
+				var params1 = { 
+					Bucket: bucket_name,
+					Delimiter: '',
+					MaxKeys : 1000,
+					Marker : Marker,
+					Delimiter: '/',
+					Prefix: "shusiou_dev/"
+				};
 
-								for (var i = 0; i < data.CommonPrefixes.length; i++) {
-									v.push(data.CommonPrefixes[i].Prefix);
-									// total_size +=  data.Contents[i].Size;
-									// file_cnt ++;
-								}
+				s3.listObjects(params1, function (err, data) {
+					if(err) {
+						getBucketsVids_callback({err:err.message});
+						return true;
+					} else {
 
-								if (data.IsTruncated) {
-									recursive_f(data.NextMarker, cbk)
+						for (var i = 0; i < data.CommonPrefixes.length; i++) {
+							v.push(data.CommonPrefixes[i].Prefix);
+							// total_size +=  data.Contents[i].Size;
+							// file_cnt ++;
+						}
 
-								} else {
-									cbk(v);
-									// cbk({file_cnt:file_cnt, total_size : total_size});
-								}
-							}
-						});						
+						if (data.IsTruncated) {
+							recursive_f(data.NextMarker, cbk)
+
+						} else {
+							cbk(v);
+							// cbk({file_cnt:file_cnt, total_size : total_size});
+						}
 					}
-					recursive_f('', getBucketsVids_callback);
-					*/
-				}
-			);
+				});						
+			}
+			recursive_f('', getBucketsVids_callback);
 		};
 		
 	};
