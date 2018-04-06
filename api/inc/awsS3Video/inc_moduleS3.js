3a2837ab16df449e7e1173d64a79a4ac856c601e
b2blosangeles <b2blosangeles@gmail.com>
2018-04-06 13:25:57 -0700

    Update inc_moduleS3.js

--- api/inc/awsS3Video/inc_moduleS3.js
+++ api/inc/awsS3Video/inc_moduleS3.js
@@ -1,26 +1,63 @@
 (function () { 
 	var obj =  function (s3, config, env, pkg) {	
 		this.getBuckets = function(getBuckets_callback) {	
-			var me = this, params = {};
+			var me = this, params = {}, Buckets = {};
 			s3.listBuckets(params, function(err, data) {
 				if(err) {
 					getBuckets_callback({err:err.message});
 					return true;
 				} else {
-					let Buckets = [],
-					    CP = new pkg.crowdProcess(),
+					let CP = new pkg.crowdProcess(),
 					    _f = {};
 					for (var i = 0; i < data.Buckets.length; i++) {
 						_f[data.Buckets[i].Name] = (function(i) {
 							return function(cbk) {
-								cbk(data.Buckets[i].Name);
+								Buckets[data.Buckets[i].Name] = {};
+								/*
+								let total_size = 0, file_cnt = 0, v = [];
+								let recursive_f = function(Marker, cbk) {
+									var params1 = { 
+										Bucket: Buckets[0].Name,
+										Delimiter: '',
+										MaxKeys : 1000,
+										Marker : Marker,
+										Delimiter: '/',
+										Prefix: "shusiou_dev/"
+									};
+
+									s3.listObjects(params1, function (err, data) {
+										if(err) {
+											getBucketsVids_callback({err:err.message});
+											return true;
+										} else {
+
+											for (var i = 0; i < data.CommonPrefixes.length; i++) {
+												v.push(data.CommonPrefixes[i].Prefix);
+												// total_size +=  data.Contents[i].Size;
+												// file_cnt ++;
+											}
+
+											if (data.IsTruncated) {
+												recursive_f(data.NextMarker, cbk)
+
+											} else {
+												cbk(v);
+												// cbk({file_cnt:file_cnt, total_size : total_size});
+											}
+										}
+									});						
+								}
+								recursive_f('', getBucketsVids_callback);								
+								*/
+								cbk(true);
 							}
 						})(i)
 					}
 					CP.serial(
 						_f,
-						function(cpresult) {				
-							getBuckets_callback(cpresult);
+						function(cpresult) {	
+							
+							getBuckets_callback(Buckets);
 						},
 						30000
 					);
