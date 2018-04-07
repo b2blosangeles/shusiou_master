f213f2902c5ae9bb69f6bd765069b1ec85c311a2
b2blosangeles <b2blosangeles@gmail.com>
2018-04-07 07:06:35 -0700

    Update inc_moduleS3.js

--- api/inc/awsS3Video/inc_moduleS3.js
+++ api/inc/awsS3Video/inc_moduleS3.js
@@ -17,13 +17,32 @@
 					getBuckets_cbk({err:err.message});
 					return true;
 				} else {
-					let list = [];
+					let astr = [], 
+					    patt = new RegExp(config.environment);
 					for (var i = 0; i <  data.Buckets.length; i++) {
 						list.push(data.Buckets[i].Name);
 					}
-					var reg = new RegExp(config.environment);
+					var connection = pkg.mysql.createConnection(config.db);
+					connection.connect();
+					let astr = [];
+					for (var i = 0; i < data.Buckets.length; i++) {
+						
+						astr.push("('" + data.Buckets.[i].Name+ "', NOW())");
+
+					}
+					var str = "INSERT INTO `cloud_spaces` (`bucket`, `updated`) VALUES " + astr.join(',');
+					connection.query(str, function (err, results, fields) {
+						connection.end();
+						if (err) {
+							getBuckets_cbk({err:err.message, tm : new Date().getTime() - tm, data:data}); 
+						} else {
+							getBuckets_cbk({tm : new Date().getTime() - tm, data:data});
+						}
+					});					
+					
+				//	var reg = new RegExp(config.environment);
 					// getBuckets_cbk(list.filter(item => reg.test(item)));
-					getBuckets_cbk(list);
+				//	getBuckets_cbk(list);
 				}
 			});	
 		}		
