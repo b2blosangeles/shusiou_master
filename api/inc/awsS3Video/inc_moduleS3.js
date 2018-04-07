33328743762c87473dec06a6316d040b916bfc8e
b2blosangeles <b2blosangeles@gmail.com>
2018-04-07 00:06:14 -0700

    Update inc_moduleS3.js

--- api/inc/awsS3Video/inc_moduleS3.js
+++ api/inc/awsS3Video/inc_moduleS3.js
@@ -12,8 +12,6 @@
 					for (var i = 0; i < data.Buckets.length; i++) {
 						_f[data.Buckets[i].Name] = (function(i) {
 							return function(cbk){
-								cbk(data.Buckets[i].Name);
-								return true;
 								me.getBucketsVids(data.Buckets[i].Name,
 									function(data) {
 										cbk(data)
