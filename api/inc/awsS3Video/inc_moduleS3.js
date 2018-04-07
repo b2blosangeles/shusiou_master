fbfe5e23c90b3141de7d9da1c0a0bb7093983fec
b2blosangeles <b2blosangeles@gmail.com>
2018-04-07 09:40:08 -0700

    Update inc_moduleS3.js

--- api/inc/awsS3Video/inc_moduleS3.js
+++ api/inc/awsS3Video/inc_moduleS3.js
@@ -55,6 +55,7 @@
 						var str1 = 'UPDATE `cloud_spaces` SET `size_info` = "'+ JSON.stringify(size_info) +'" ' +
 						    " SET `updated` = NEW() " +
 						    " WHERE `bucket` = '"+ results[0].bucket +"'; ";
+						connection.connect();
 						connection.query(str1, function (err, results, fields) {
 							connection.end();
 							updateBucket_cbk({size_info:size_info});
