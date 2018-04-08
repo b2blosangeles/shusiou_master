f862e4ec8fffad23ae17bd04cfbdc2223d47dd2b
b2blosangeles <b2blosangeles@gmail.com>
2018-04-07 23:19:55 -0700

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -3,7 +3,7 @@
 		
 		let _space = { 
 			space_id : 'shusiou-dev-2',
-			space_url :'https://shusiou-dev-1.nyc3.digitaloceanspaces.com/',
+			space_url :'https://shusiou-dev-2.nyc3.digitaloceanspaces.com/',
 			mnt_folder : '/var/shusiou_video/'
 		};
 
