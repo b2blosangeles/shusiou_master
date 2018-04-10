0c4c09f5ef19857f627e95b5e4c7ed41fddb9ab9
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 13:56:08 -0700

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -38,40 +38,7 @@ function s() {
 s();
 
 /* --- code for cron watch ---*/
-(function(tp, scheduled){
-    var path = require('path'),
-	watch_file = '/var/.qalet_cron_watch.data';
-	env = {root_path:path.join(__dirname, '../../..')},
-	fn_a = /\/([^\/]+)$/i.exec(__filename),
-	cron_data = require(env.root_path + '/sites/' + tp + '/cron_service/cron.json');
-	
-	var script_name = '';
-	for (var i = 0; i < cron_data.length; i++) {
-		if ( cron_data[i].script == fn_a[1]) {
-			script_name = fn_a[1];
-		}
-	}
-	
-    var request =  require(env.root_path + '/package/request/node_modules/request');
-    var fs = require('fs');
-
-	fs.readFile(watch_file, 'utf8', function(err,data) {
-		if (err){
-			fs.writeFile(watch_file, JSON.stringify({}), function (err) {});
-		} else {
-			var watch = {};
-			try { watch = JSON.parse(data);} catch (e) {}
-			
-			let start = ((watch[tp + '_'+ fn_a[1]]) && (watch[tp + '_'+ fn_a[1]].mark)) ? watch[tp + '_'+ fn_a[1]].mark : null;
-			
-			if (script_name) {
-				watch[tp + '_'+ script_name] = {scheduled:scheduled, start: start, mark:new Date()};
-			} else {
-				delete watch[tp + '_'+ fn_a[1]];
-			}
-			fs.writeFile(watch_file, JSON.stringify(watch), function (err) {
-				console.log(watch);
-			});
-		}
-	});	
-})('master', 60);
+delete require.cache[__dirname + '/watch_cron.inc.js'];
+let watch_cron_inc = require(__dirname + '/watch_cron.inc.js'),
+    watchCron = new watch_cron_inc(__filename);
+watchCron.load('master', 60);
