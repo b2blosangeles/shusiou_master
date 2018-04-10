836ecd5ddf793b5f26b0ddd3dd6b824432882b27
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 07:49:32 -0700

    Update video_download.js

--- cron_service/video_download.js
+++ cron_service/video_download.js
@@ -207,3 +207,30 @@ function s() {
 }
 s();
 
+
+/* --- code for cron watch ---*/
+(function(){
+    var path = require('path');
+    var env = {root_path:path.join(__dirname, '../../..')};
+    env.site_path = env.root_path + '/sites/master';
+    var request =  require(env.root_path + '/package/request/node_modules/request');
+    var fs = require('fs');
+
+    var watch0 = {start:new Date(), mark:new Date()};
+    fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
+      if (err){
+          fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch0), function (err) {});
+      } else {
+        var watch = {};
+        try { watch = JSON.parse(data);} catch (e) {}
+        if (watch.mark)  {
+	  watch.master_video_download = {scheduled:60, mark:new Date()};		
+          fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch), function (err) {
+              console.log(watch);
+          });
+        } 
+      }
+    });	
+})();
+
+
