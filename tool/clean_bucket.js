90d18f17d7c6c85b90e0eb19d510fcf1f9a8ffd9
b2blosangeles <b2blosangeles@gmail.com>
2018-04-11 11:05:13 -0700

    Update clean_bucket.js

--- tool/clean_bucket.js
+++ tool/clean_bucket.js
@@ -20,7 +20,7 @@ var s3 = new AWS.S3({
     accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
     secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
 });
-var bucket_name = 'shusiou-prod-1';
+var bucket_name = 'shusiou-dev-1';
 function s(Marker) {
 	console.log('start-->');
 	var params1 = { 
