07d9684f20bdd4d22ee958072e9069d12c468e4d
b2blosangeles <b2blosangeles@gmail.com>
2018-04-12 21:38:53 -0700

    Update clean_bucket.js

--- tool/clean_bucket.js
+++ tool/clean_bucket.js
@@ -16,7 +16,7 @@ let pkg = {
 }; 
 const AWS = require(env.site_path + '/api/inc/aws-sdk/node_modules/aws-sdk');
 var s3 = new AWS.S3({
-    httpOptions: {timeout: 5000},		
+    httpOptions: {timeout: 50000},		
     endpoint: new AWS.Endpoint('nyc3.digitaloceanspaces.com'),
     accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
     secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
