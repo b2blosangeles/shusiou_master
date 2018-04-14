99378510153a165225d06928560de99475904ea5
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 08:01:16 -0700

    Update embed_curriculum_demo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_demo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_demo.jsx
@@ -22,7 +22,7 @@ try {
 		render: function() {
 			var me = this;
 			if ((me.props.params.id) && (me.props.parent.state.curriculum)) {
-				return (<div>Embed_curriculum_demo 1 ==<_commonLib data={{rec:me.props.parent.state.curriculum}}/>
+				return (<div>Embed_curriculum_demo 1 ==<_commonLib data={{rec:{}}}/>
 						<div>
 							<h4>{me.props.parent.state.video.title}</h4>	
 							<p><b>Video ID</b>:{me.props.parent.state.curriculum.vid}</p>  
