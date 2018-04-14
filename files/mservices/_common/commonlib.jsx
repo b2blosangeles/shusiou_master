7debc71bbc1381418f2b71c039715c13858d4a78
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 07:31:20 -0700

    Update commonlib.jsx

--- files/mservices/_common/commonlib.jsx
+++ files/mservices/_common/commonlib.jsx
@@ -1,9 +1,24 @@
 try {
-	var _commonLib =  {
+	var _commonLib = React.createClass({
 		videoImageUrl : function() {
 			return 'aaa';
+		},
+		render: function() {
+			var me = this;
+			if ((me.props.params.id) && (me.props.parent.state.curriculum)) {
+				return (<div>Embed_curriculum_demo 1 =={_commonLib.videoImageUrl()}
+						<div>
+							<h4>{me.props.parent.state.video.title}</h4>	
+							<p><b>Video ID</b>:{me.props.parent.state.curriculum.vid}</p>  
+							<p><b>Video Length</b>:({me.props.parent.state.curriculum.video_length} Secs)</p>
+							<img src={me.videoImage(61, me.props.parent.state.curriculum)}/></div>	
+					
+					</div>)
+			} else {
+				return (<div>Embed_curriculum_preview 2</div>)
+			}
 		}
-	};	
+	});	
 } catch (err) {
 	console.log(err.message);
 }
