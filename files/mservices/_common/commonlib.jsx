824e8dc35b460e109203671e9831eaf0aaa574a3
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 08:08:40 -0700

    Update commonlib.jsx

--- files/mservices/_common/commonlib.jsx
+++ files/mservices/_common/commonlib.jsx
@@ -6,6 +6,7 @@ try {
 			return {};
 		},		
 		videoImageUrl : function() {
+			var me = this;
 			let a = me.props.data.rec, t =  me.props.data.t;
 			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
 				      '&size=480&ss='+t;
