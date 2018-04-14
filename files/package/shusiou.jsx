599d40ca99db2ca42f0162cdfeb74ada65b30214
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 07:02:58 -0700

    Update shusiou.jsx

--- files/package/shusiou.jsx
+++ files/package/shusiou.jsx
@@ -13,7 +13,6 @@
 			"/mservices/_common/modal-backdrop.js",
 			"/mservices/_common/modal.jsx",
 			"/mservices/_common/breakpoints.jsx",
-			"/mservices/_common/commonlib.jsx",
 			
 			"/mservices/react_module/lang_space/controller/shusiou_config.js",			
 			
