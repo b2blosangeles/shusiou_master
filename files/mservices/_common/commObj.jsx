b66762605c5c639b6375a281463605f67a163206
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 15:14:17 -0700

    Update commObj.jsx

--- files/mservices/_common/commObj.jsx
+++ files/mservices/_common/commObj.jsx
@@ -14,16 +14,6 @@ try {
 			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
 				      '&size=' + size + '&ss=' + t;
 			return (<img src={url}/>);
-		},
-		videoImageA : function() {
-			var me = this;
-			let a = me.props.data.rec, 
-			    t =  me.props.data.t, 
-			    size =  (me.props.data.size) ?  me.props.data.size : 480;
-			
-			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
-				      '&size=' + size + '&ss=' + t;
-			return (<img src={url}/>);
 		},		
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.data.code : '';
