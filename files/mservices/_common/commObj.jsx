2a50295cce07ba56f913dfb8bcd5622eb9947ace
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 15:37:52 -0700

    Update commObj.jsx

--- files/mservices/_common/commObj.jsx
+++ files/mservices/_common/commObj.jsx
@@ -14,6 +14,18 @@ try {
 			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
 				      '&size=' + size + '&ss=' + t;
 			return (<img src={url}/>);
+		},
+		video : function() {
+			var me = this;
+			let a = me.props.data.rec, 
+			    t =  me.props.data.t, 
+			    size =  (me.props.data.size) ?  me.props.data.size : 480;
+			
+			var url =  _node_svr() + '/api/video/pipe_stream.api?space=' + a.space + '&video_fn='+ a.vid +
+				      '&ss=' + t + '&t=30';
+			return (<video src={url} poster="movie.jpg" controls>
+				This is fallback content to display for user agents that do not support the video tag.
+			</video>);
 		},		
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.code : '';
