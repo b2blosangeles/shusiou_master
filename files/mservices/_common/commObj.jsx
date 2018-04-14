3b14462c15ab4b4068cf7605efcc03798fbff50a
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 15:20:07 -0700

    Update commObj.jsx

--- files/mservices/_common/commObj.jsx
+++ files/mservices/_common/commObj.jsx
@@ -19,7 +19,7 @@ try {
 			let me = this, code = (me.props.data) ? me.props.data.code : '';
 			// return me[code]();
 			if (me[code]) {
-				return (me[code])
+				return me[code]
 			} else {
 				return 'inorrect code! ' + code
 			}	
