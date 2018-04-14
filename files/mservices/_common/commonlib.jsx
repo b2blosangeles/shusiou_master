ae60f6aab7dca772d53e24bf4f288b9473b11551
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 07:32:10 -0700

    Update commonlib.jsx

--- files/mservices/_common/commonlib.jsx
+++ files/mservices/_common/commonlib.jsx
@@ -5,18 +5,7 @@ try {
 		},
 		render: function() {
 			var me = this;
-			if ((me.props.params.id) && (me.props.parent.state.curriculum)) {
-				return (<div>Embed_curriculum_demo 1 =={_commonLib.videoImageUrl()}
-						<div>
-							<h4>{me.props.parent.state.video.title}</h4>	
-							<p><b>Video ID</b>:{me.props.parent.state.curriculum.vid}</p>  
-							<p><b>Video Length</b>:({me.props.parent.state.curriculum.video_length} Secs)</p>
-							<img src={me.videoImage(61, me.props.parent.state.curriculum)}/></div>	
-					
-					</div>)
-			} else {
-				return (<div>Embed_curriculum_preview 2</div>)
-			}
+			return (<div>niu</div>)
 		}
 	});	
 } catch (err) {
