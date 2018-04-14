b91c1bf38b1bddf973b566b9fb5e2b8d7b234190
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 07:58:08 -0700

    Update commonlib.jsx

--- files/mservices/_common/commonlib.jsx
+++ files/mservices/_common/commonlib.jsx
@@ -1,5 +1,10 @@
 try {
 	var _commonLib = React.createClass({
+		getInitialState: function() {
+			console.log(me.props.data);
+			var me = this;
+			return {};
+		},		
 		videoImageUrl : function(a, t) {
 			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
 				      '&size=480&ss='+t;
