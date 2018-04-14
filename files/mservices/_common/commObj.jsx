a375ce090a6d7762c1ed69439286ae7e7271b005
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 15:12:03 -0700

    Update commObj.jsx

--- files/mservices/_common/commObj.jsx
+++ files/mservices/_common/commObj.jsx
@@ -27,7 +27,8 @@ try {
 		},		
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.data.code : '';
-			return (typeOf me[code] === 'function') ? me[code]() : ('inorrect code! ' + code);
+			return me[code]();
+			// return (typeof me[code] === 'function') ? me[code]() : ('inorrect code! ' + code);
 		}
 	});	
 } catch (err) {
