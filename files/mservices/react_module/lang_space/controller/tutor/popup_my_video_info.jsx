5b1901cc32fb47c1e016b3fba859fe18aee01acf
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 22:35:32 -0700

    Update popup_my_video_info.jsx

--- files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
@@ -16,7 +16,10 @@ try {
 			var me = this;
 			if (me.props.rec.vid !== prePropos.rec.vid) me.setState({rec:me.props.rec});
 			console.log('---------componentDidUpdate--------');
+			console.log('me.props.rec->');
 			console.log(me.props.rec);
+			console.log('prePropos.rec->');
+			console.log(prePropos.rec);
 		},			
 		close_admin:function(){
 			var me = this;  // $('video').attr('src', ''); 
