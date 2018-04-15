516e952e58f4cd94a7ce217b27479b470a2e6d1a
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 23:43:45 -0700

    Update popup_my_video_info.jsx

--- files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
@@ -25,16 +25,9 @@ try {
 		},
 		loadVideo:function() {
 			var me = this; 
-			let rec = me.props.rec;
-			return (<span>{rec.vid}=={me.state.rec.vid}</span>)
-			/*
-			return (function(rec) {
-				//return function() {
-					return (<_commObj code={'video'} data={{rec : rec, 
+			// return (<span>{rec.vid}=={me.state.rec.vid}</span>)
+			return (<_commObj code={'video'} data={{rec : me.props.rec, 
 							size:800}}/>);
-				//}	
-			})(rec);
-			*/
 		},
 		render:function() {
 			var me = this;
