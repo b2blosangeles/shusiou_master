bb7f253a13120e6693a9d49e22d3429954039202
b2blosangeles <b2blosangeles@gmail.com>
2018-04-14 22:00:45 -0700

    Update popup_my_video_info.jsx

--- files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/popup_my_video_info.jsx
@@ -6,17 +6,7 @@ try {
 			return {};
 		},
 		componentDidMount:function() {
-			var me = this;
-/*
-			$('.pop_preview')[0].src = me.videoLink();
-			$('.pop_preview')[0].play();
-*/			
-		},
-		componentDidUpdate:function(prePropos, preState) {
-			var me = this;
-			/*
-			$('.pop_preview')[0].src = me.videoLink();
-			$('.pop_preview')[0].play();*/			
+			var me = this;		
 		},		
 		close_admin:function(){
 			var me = this;  $('video').attr('src', ''); me.props.parent.closeAdmin();
@@ -26,45 +16,33 @@ try {
 			me.props.parent.videoDelete(me.props.rec.vid);
 			me.close_admin();
 			
-		},		
-		videoLink:function(){
-			let me = this,
-			    r = me.props.rec;
-			return  _node_svr() +  '/api/video/pipe_stream.api?env=' + r.env + '&space=' + r.space + '&video_fn='+ r.vid;			
-		},			
+		},					
 		render:function() {
 			var me = this;
 			return (
-				<p>
-					{/*
-					<button type="button" className="close pull-right"  
-						onClick={me.close_admin.bind(me)}
-						>&times;</button>
-					*/}	
-					<div className="container">
-						<div className="row">
-							<h4>{me.props.rec.title}</h4>
-						</div>
-						<div className="row">
-							<div style={{'padding-bottom':'0.5em'}}>
-								<button type="button" className="btn btn-danger"
-									onClick={me.delete_video.bind(me)}>
-								<i className="fa fa-trash-o" aria-hidden="true"></i> Delete the video
-								</button>
-								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								<button type="button" className="btn btn-info"
-									onClick={me.close_admin.bind(me)}>
-								<i className="fa fa-close" aria-hidden="true"></i> Close
-								</button>
-							</div>	
+				<div className="container">
+					<div className="row">
+						<h4>{me.props.rec.title}</h4>
+					</div>
+					<div className="row">
+						<div style={{'padding-bottom':'0.5em'}}>
+							<button type="button" className="btn btn-danger"
+								onClick={me.delete_video.bind(me)}>
+							<i className="fa fa-trash-o" aria-hidden="true"></i> Delete the video
+							</button>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<button type="button" className="btn btn-info"
+								onClick={me.close_admin.bind(me)}>
+							<i className="fa fa-close" aria-hidden="true"></i> Close
+							</button>
 						</div>	
-						<div className="row">
-							<_commObj code={'video'} data={{rec:me.props.rec, 
-								size:800}}/>
-														
-						</div>							
 					</div>	
-				</p>
+					<div className="row">
+						<_commObj code={'video'} data={{rec:me.props.rec, 
+							size:800}}/>
+
+					</div>							
+				</div>	
 			)	
 		}
 	});	
