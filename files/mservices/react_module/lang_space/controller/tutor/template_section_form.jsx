b91061374dad72cb470b84e3a6ace15b4ee61cc1
b2blosangeles <b2blosangeles@gmail.com>
2018-04-19 14:43:35 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -125,7 +125,7 @@ try {
 		saveSection:function(opt){
 			let me = this, 
 			    data = {section_id:me.props.section_id, tpl:me.state.c_tpl, data:me.state.data, c_section:me.state.c_section};
-			alert(JSON.stringify(data));
+			alert( _master_svr() + '/api/curriculum/myCurriculum.api');
 			
 			me.props.env.engine({
 				url: _master_svr() + '/api/curriculum/myCurriculum.api',
