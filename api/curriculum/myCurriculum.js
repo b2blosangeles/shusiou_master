3cb0452644e8dbd26d788e58d34c43d0218483c5
b2blosangeles <b2blosangeles@gmail.com>
2018-04-19 21:05:44 -0700

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -46,7 +46,6 @@ var app = function(auth_data) {
 					
 				});  
 			};
-			/*
 			_f['P0'] = function(cbk) {
 				let v = CP.data.S0;
 
@@ -127,7 +126,6 @@ var app = function(auth_data) {
 					
 				});  
 			};
-			*/
 			CP.serial(
 				_f,
 				function(data) {
