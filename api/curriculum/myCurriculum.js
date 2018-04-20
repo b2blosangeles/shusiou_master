eff45a91fbc4c80ac65047db01e28b6d65784d6f
b2blosangeles <b2blosangeles@gmail.com>
2018-04-20 06:14:29 -0700

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -66,12 +66,15 @@ var app = function(auth_data) {
 					}					
 					
 				} else if (opt === 'deleteSection')  {
-					cbk('--lv-2--' + opt);
-					return true;	
+
 					var lv = v.filter(function(a) {
 						return a.section_id != req.body.data.section.section_id;
 					});
 					v = lv;
+					
+					cbk('--lv-2--' + v);
+					return true;						
+					
 				}
 				
 				v.sort(function(a1, a2) {
