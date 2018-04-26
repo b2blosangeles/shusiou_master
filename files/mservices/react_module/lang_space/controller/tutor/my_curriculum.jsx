2c3ddfc57d9bcc880a7cca1883e23df2f060791b
b2blosangeles <b2blosangeles@gmail.com>
2018-04-26 14:42:14 -0700

    Update my_curriculum.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
@@ -116,19 +116,20 @@ try {
 		componentDidUpdate:function() {
 			var me = this;
 		},
+		closePopup:function() {
+			var me = this;
+			me.setState({ModalPlus:'cancel'});			
+			return true;
+		},			
 		deleteCurriculum: function(params, track) {
 			var me = this;
-			// me.sendDeleteCurriculum();
-			//return true;
 			me.setState({ModalPlus:{type:'popup',  hold:0,
 				box_style:{top:'28px'},
 				header: (<span/>),		
 				message: (<div className="container-fluid">
 						<p>It is going to clean up the curriculum please confirm:</p>
 						<button className="btn btn-danger btn_margin6 pull-right" onClick={me.sendDeleteCurriculum.bind(me)}>Confirm</button>
-						{/*
 						<button className="btn btn-warning btn_margin6 pull-right" onClick={me.closePopup.bind(me)}>Cancel</button>
-						*/}
 					</div>),
 				footer:(<span/>)
 			}});			
