dcdacd582d662ee733e458c9fbf9b3df2562bab6
b2blosangeles <b2blosangeles@gmail.com>
2018-04-27 23:03:35 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -15,10 +15,12 @@ try {
 		},		
 		componentDidUpdate:function(prePropos, prevStat) {
 			var me = this;
+			me.setState({niu:'BBB'});
 		//	if (me.props.parent.state.niu != me.props.parent.state.niu) {
 				console.log('me.props.parent.state.niu ===>');
 				console.log(me.props.parent.state.niu);
 		//	}
+			console.log(me.State);
 			console.log(prevStat);
 		},		
 		render: function() {
