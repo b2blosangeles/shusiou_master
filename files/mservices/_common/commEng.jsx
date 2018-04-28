26f34a7f6436a98cd80833e5085541a9d4d771e7
b2blosangeles <b2blosangeles@gmail.com>
2018-04-28 15:55:47 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -7,7 +7,7 @@ try {
 		postData: function() {
 			var me = this;
 			alert('me.props.parent.state.eng.shift()');
-			// me.props.parent.setState({eng:me.props.parent.state.eng});
+			me.props.parent.setState({eng:me.props.parent.state.eng});
 			return true;
 		},
 		componentDidMount:function() {
@@ -16,9 +16,9 @@ try {
 		},		
 		componentDidUpdate:function(prePropos, prevStat) {
 			var me = this;
-			//if (me.props.parent.state.eng && me.props.parent.state.eng.length) {
+			if (me.props.parent.state.eng && me.props.parent.state.eng.length) {
 				me.postData();
-			//}
+			}
 		},		
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.code : '';
