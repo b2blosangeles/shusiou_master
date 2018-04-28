81f8d66ad3d2499c725e8b4b5951f7bae8be1cd3
b2blosangeles <b2blosangeles@gmail.com>
2018-04-27 22:18:57 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -3,7 +3,19 @@ try {
 		getInitialState: function() {
 			var me = this;
 			return {};
-		},	
+		},
+		postData: function() {
+			var me = this;
+			alert(123);
+			return true;
+		},
+		componentDidMount:function() {
+			var me = this;
+			me.postData();
+		},		
+		componentDidUpdate:function() {
+			var me = this;
+		},		
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.code : '';
 			return (<span> -- test --</span>)
