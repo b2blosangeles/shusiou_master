036be4228fc9b89e796e991a2ad287321ac32777
b2blosangeles <b2blosangeles@gmail.com>
2018-04-29 07:43:36 -0700

    Update modal.jsx

--- files/mservices/_common/modal.jsx
+++ files/mservices/_common/modal.jsx
@@ -234,7 +234,7 @@ try {
 			}
 			var message = '', box_style={};
 			var message = (me.state.ModalLoading.message)?(me.state.ModalLoading.message):'Loading ...';
-			box_style = (me.state.ModalLoading.boxstyle)?me.state.ModalLoading.boxstyle:{color:'#000000'};
+			box_style = (me.state.ModalLoading.boxstyle)?me.state.ModalLoading.boxstyle:{color:'#ffffff'};
 
 			return (			
 				<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
