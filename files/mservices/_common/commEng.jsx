ee6db576b036ec83a749b6419a2be8788af23a3b
b2blosangeles <b2blosangeles@gmail.com>
2018-04-28 23:19:42 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -6,87 +6,12 @@ try {
 		},
 		cp: function() {
 			let me = this, s = me.props.parent.state.eng.s, p = me.props.parent.state.eng.p;
-			let crowdProcess =  function () {
-				this.serial = function(q, cbk, timeout) {
-					var me = this;
-					var idx = '', tm = new Date().getTime();
-					var vtime = (isNaN(timeout) || timeout == 0)?6000:timeout
-					me.data = {};	
-					var _f = function(o) {
-						return function(res) {
-							delete q[o];
-							idx = '';
-							me.data[o] = res;
-						}
-					}
-					var _itv = setInterval(
-						function(){
-							if (!idx) {
-								if (!Object.keys(q).length) {
-									clearInterval(_itv);
-									cbk({_spent_time:new Date().getTime() - tm, status:'success', results:me.data});
-								} else {
-									idx = Object.keys(q)[0];
-									if ((q[idx]) && typeof q[idx] == 'function') {
-										if (!me.exit) {
-											q[idx](_f(idx));
-										} else {
-											delete q[idx];
-											idx = '';
-										}
-									} 
-								}
-							}
-							if (new Date().getTime() - tm > vtime) {
-								clearInterval(_itv);
-								cbk({_spent_time:new Date().getTime() - tm, status:'timeout', results:me.data});
-							}				
-							return true;
-						}
-					, 1); 
-				};
-				this.parallel = function(q, cbk, timeout) {
-					var me = this;
-					var tm = new Date().getTime(), vtime = (isNaN(timeout) || timeout == 0)?6000:timeout;
-
-					me.data = {};	
-					var count_q = 0, count_r = 0;
-					for (var o in q) {
-						count_q++;	
-						var _f = function(o) {
-							return function(res) {
-								count_r++;
-								me.data[o] = res;
-							}
-						}
-						if ((q[o]) && typeof q[o] == 'function') {
-							q[o](_f(o));
-						} 						
-					}
-					var _itv = setInterval(
-						function(){			
-							if (count_q == count_r) {
-								clearInterval(_itv);
-								cbk({_spent_time:new Date().getTime() - tm, status:'success', results:me.data});
-								console.log(new Date());
-							}
-							if (new Date().getTime() - tm > vtime) {
-								clearInterval(_itv);
-								cbk({_spent_time:new Date().getTime() - tm, status:'timeout', results:me.data});
-								console.log(new Date());
-							}				
-							return true;
-						}
-					, 1); 		
-				};
-			};
-			
-			let CP0 = new crowdProcess(), CP = new crowdProcess();
+			let CP0 = new me.crowdProcess(), CP = new me.crowdProcess();
 			let qp = {};
 			for (var j = 0; j < p.length; j++) {
 				qp['P_'+j] = (function(j) {
 					return function(cbk) {
-						cbk('p[j]---');
+						cbk(p[j]);
 					}
 				})(j);
 			}
@@ -101,7 +26,7 @@ try {
 					6000);	
 			};			
 			for (var i = 0; i < s.length; i++) {
-				qs['S1_'+i] = (function(i) {
+				qs['S_'+i] = (function(i) {
 					return function(cbk) {
 						cbk(s[i]);
 					}
@@ -115,6 +40,80 @@ try {
 				30000);
 			return true;
 		},
+		crowdProcess :  function () {
+			this.serial = function(q, cbk, timeout) {
+				var me = this;
+				var idx = '', tm = new Date().getTime();
+				var vtime = (isNaN(timeout) || timeout == 0)?6000:timeout
+				me.data = {};	
+				var _f = function(o) {
+					return function(res) {
+						delete q[o];
+						idx = '';
+						me.data[o] = res;
+					}
+				}
+				var _itv = setInterval(
+					function(){
+						if (!idx) {
+							if (!Object.keys(q).length) {
+								clearInterval(_itv);
+								cbk({_spent_time:new Date().getTime() - tm, status:'success', results:me.data});
+							} else {
+								idx = Object.keys(q)[0];
+								if ((q[idx]) && typeof q[idx] == 'function') {
+									if (!me.exit) {
+										q[idx](_f(idx));
+									} else {
+										delete q[idx];
+										idx = '';
+									}
+								} 
+							}
+						}
+						if (new Date().getTime() - tm > vtime) {
+							clearInterval(_itv);
+							cbk({_spent_time:new Date().getTime() - tm, status:'timeout', results:me.data});
+						}				
+						return true;
+					}
+				, 1); 
+			};
+			this.parallel = function(q, cbk, timeout) {
+				var me = this;
+				var tm = new Date().getTime(), vtime = (isNaN(timeout) || timeout == 0)?6000:timeout;
+
+				me.data = {};	
+				var count_q = 0, count_r = 0;
+				for (var o in q) {
+					count_q++;	
+					var _f = function(o) {
+						return function(res) {
+							count_r++;
+							me.data[o] = res;
+						}
+					}
+					if ((q[o]) && typeof q[o] == 'function') {
+						q[o](_f(o));
+					} 						
+				}
+				var _itv = setInterval(
+					function(){			
+						if (count_q == count_r) {
+							clearInterval(_itv);
+							cbk({_spent_time:new Date().getTime() - tm, status:'success', results:me.data});
+							console.log(new Date());
+						}
+						if (new Date().getTime() - tm > vtime) {
+							clearInterval(_itv);
+							cbk({_spent_time:new Date().getTime() - tm, status:'timeout', results:me.data});
+							console.log(new Date());
+						}				
+						return true;
+					}
+				, 1); 		
+			};
+		},		
 		componentDidMount:function() {
 			var me = this;
 			
