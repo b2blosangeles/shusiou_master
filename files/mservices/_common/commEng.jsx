aeb3484f0ed3def0788e44c641d6d23e16df05c3
b2blosangeles <b2blosangeles@gmail.com>
2018-04-29 07:24:21 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -171,7 +171,7 @@ try {
 		},
 		loading:function() {
 			var me = this;
-			me.setState({ModalLoading: {color:'#000000', hold:1000, 
+			me.setState({ModalLoading: {box_style:{color:'#000000'}, hold:1000, 
 				message:'<img src="https://i.stack.imgur.com/oQ0tF.gif" width="24">'}});
 			setTimeout(
 				function() {
