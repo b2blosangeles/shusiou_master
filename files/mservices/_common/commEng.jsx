05b822d2808afb2cec117d423e80952f58d66e55
b2blosangeles <b2blosangeles@gmail.com>
2018-04-30 08:03:02 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -154,7 +154,7 @@ try {
 			var me = this;
 			if (me.props.parent.state.eng && me.props.parent.state.eng.p && me.props.parent.state.eng.p.length) {
 				// if (!me.state.ModalLoading || !me.state.ModalLoading.id) {
-				if (!me.state.ModalLoading.id || me.state.ModalLoading.id !== prevStat.ModalLoading.id) {
+				if (!me.state.ModalLoading || me.state.ModalLoading.id !== prevStat.ModalLoading.id) {
 					me.cpCall();
 				}
 			}
