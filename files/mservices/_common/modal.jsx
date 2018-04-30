a4d17e1601d9c11854c6373e50a2bab34629af7f
b2blosangeles <b2blosangeles@gmail.com>
2018-04-29 22:02:06 -0700

    Update modal.jsx

--- files/mservices/_common/modal.jsx
+++ files/mservices/_common/modal.jsx
@@ -185,9 +185,7 @@ try {
 			if (me.props.parent.state.ModalLoading.id) {
 				me.M = me.props.parent.state.ModalLoading
 			}
-			console.log('====M====');
-			console.log(me.M);
-			
+
 			if ((me.props.parent) && (me.props.parent.state.ModalLoading)) {	
 				if (me.props.parent.state.ModalLoading == 'cancel') {
 					viewpoint.find('.ModalLoading').modal('hide');
