82a4f5e4b3405faad55acd12053f373c80b50b67
b2blosangeles <b2blosangeles@gmail.com>
2018-05-01 16:09:30 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -7,7 +7,9 @@ try {
 		componentDidUpdate:function(prePropos, prevStat) {
 			var me = this;
 			if (me.props.parent.state.ModalPopup) {
-				me.setState({ModalPopup:me.props.parent.state.ModalPopup})
+				
+				me.setState({ModalPopup:me.props.parent.state.ModalPopup});
+				me.props.parent.state({ModalPopup:null});
 				alert('me.props.parent.state.ModalPopup');
 				//if (!me.state.ModalPopup) {
 				//	alert('me.cpCall()');
