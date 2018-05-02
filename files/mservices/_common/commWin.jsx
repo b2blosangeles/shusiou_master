c8a76d334147e6ba0675de37f101d58fd7afbbf6
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 08:54:07 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -33,35 +33,24 @@ try {
 		},		
 		render: function() {
 			let me = this, ModalPopup = (me.props.parent.state.ModalPopup) ? me.props.parent.state.ModalPopup : null;
-			//if (!ModalPopup || ModalPopup === 'cancel') 
-			//	return (<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true"/>);
-			//else {
+			if (!ModalPopup || ModalPopup === 'cancel') 
+				return (<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true"/>);
+			else {
 				var box_class = 'danger', 
 				    message = ModalPopup.message, 
 				    box_style={}, close_icon = '';
 				// close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
-				return (			
-					<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
-					{
-						if (!ModalPopup || ModalPopup === 'cancel') return (<span/>)
-						else {
-							return(
-							  <div className="modal-dialog modal-lg" role="document">
-								<div className={'alert alert-' + box_class} style={box_style} role="alert">
-									<span dangerouslySetInnerHTML={{__html: message}}></span>
-									<button type="button" className="close" 
-										onClick={me.closePopup.bind(me)}
-										style={{display:close_icon}}>
-										&times;
-									</button>
-								</div>
-							  </div>
-							)
-						}	
-					}
-					</div>	
-				);
-			//}
+				  <div className="modal-dialog modal-lg" role="document">
+					<div className={'alert alert-' + box_class} style={box_style} role="alert">
+						<span dangerouslySetInnerHTML={{__html: message}}></span>
+						<button type="button" className="close" 
+							onClick={me.closePopup.bind(me)}
+							style={{display:close_icon}}>
+							&times;
+						</button>
+					</div>
+				  </div>
+			}
 		}		
 		
 	});	
