2ced4e8eaed114703d436e1cd25bb99a5690f0e9
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 07:05:58 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -194,7 +194,7 @@ try {
 			_EngIndex = (!_EngIndex || _EngIndex > 10000) ? 1 : (_EngIndex + 1);
 			me.setState({ModalLoading: {id : _EngIndex, box_style : {color:'#ffffff'}, hold:10, 
 				message:'<img src="' + _master_svr() + '/images/loading_spin.gif" width="24">'}},
-				functon() {
+				function() {
 					console.log('_EngIndex_' + _EngIndex);
 					viewpoint.find('.ModalLoading_' + me.state.ModalLoading.id).modal({backdrop:'static'});				    
 				    }	   
