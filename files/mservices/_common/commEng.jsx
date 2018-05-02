92fa220b3e8eb4eb00632ee83d164136e7beae83
b2blosangeles <b2blosangeles@gmail.com>
2018-05-01 22:44:40 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -219,6 +219,7 @@ try {
 		*/
 		loading:function() {
 			var me = this;
+			return true;
 			me._idx = (!me._idx || me._idx > 10000) ? 1 : (me._idx + 1);
 			me.setState({ModalLoading: {id : me._idx, box_style : {color:'#ffffff'}, hold:10, 
 				message:'<img src="' + _master_svr() + '/images/loading_spin.gif" width="24">'}});
