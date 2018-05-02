6313cf381dd24da2cbefafc3c2a8917c38ae8aec
b2blosangeles <b2blosangeles@gmail.com>
2018-05-01 20:29:46 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -10,11 +10,43 @@ try {
 				me.setState({ModalPopup:me.props.parent.state.ModalPopup});
 				me.props.parent.setState({ModalPopup:null});
 			} 
-		},		
+		},
+		/*
 		render: function() {
 			let me = this, code = (me.props.data) ? me.props.code : '';
 			return (<ModalPopup parent={me} />)
 		}
+		*/
+		closePopup : function() {
+			var me = this;
+			me.props.parent.setState({ModalPopup:'cancel'});
+		},		
+		ModalLoadingClass: function() {
+			let me = this;	
+			return 'modal fade ModalPopup';
+		},		
+		render: function() {
+			var me = this, err_msg = '';
+			var box_class = 'danger', 
+			    message = (me.props.parent.state.ModalPopup) ? me.props.parent.state.ModalPopup.message : 'test 122', 
+			    box_style={}, close_icon = '';
+			// close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
+			return (			
+				<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
+					  <div className="modal-dialog modal-lg" role="document">
+						<div className={'alert alert-' + box_class} style={box_style} role="alert">
+							<span dangerouslySetInnerHTML={{__html: message}}></span>
+							<button type="button" className="close" 
+								onClick={me.closePopup.bind(me)}
+								style={{display:close_icon}}>
+								&times;
+							</button>
+						</div>
+					  </div>
+				</div>	
+			);
+		}		
+		
 	});	
 } catch (err) {
 	console.log(err.message);
