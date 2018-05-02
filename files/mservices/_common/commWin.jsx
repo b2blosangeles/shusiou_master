0e62c09bb5f92a129e9a851bc8a597d871037c7c
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 13:42:42 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -10,10 +10,10 @@ try {
 			if (me.props.parent.state.ModalPopup === 'cancel') {
 				me.props.parent.setState({ModalPopup:null});
 				//me.render();
-				viewpoint.find('.ModalPopup').modal('hide');
+				viewpoint.find('.ModalPopup_' + me.state.id).modal('hide');
 			} else if (me.props.parent.state.ModalPopup) {
 				//me.render();
-				viewpoint.find('.ModalPopup').modal({backdrop:'static'});
+				viewpoint.find('.ModalPopup_' + me.state.id).modal({backdrop:'static'});
 			}			
 		},
 		closePopup : function() {
@@ -33,7 +33,7 @@ try {
 		},
 		ModalLoadingClass: function() {
 			let me = this;	
-			return 'modal fade ModalPopup';
+			return 'modal fade ModalPopup_' + me.state.id;
 		},		
 		render: function() {
 			let me = this, ModalPopup = (me.props.parent.state.ModalPopup) ? me.props.parent.state.ModalPopup : {};
