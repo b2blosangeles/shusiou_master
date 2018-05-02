6d43efcda3f885ccbe988aa54f4c1abb977d78b9
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 15:39:30 -0700

    Update qalet_plugin.js

--- files/package/qalet_plugin.js
+++ files/package/qalet_plugin.js
@@ -11,7 +11,7 @@
 	],
 	"mservice": {	
 		"lang_space":[
-			"/mservices/_common/lib.js",
+			"/mservices/_common/commLib.js",
 			"/mservices/_common/modal-backdrop.js",
 			"/mservices/_common/modal.jsx",
 			"/mservices/_common/breakpoints.jsx",
