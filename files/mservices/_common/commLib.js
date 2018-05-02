e02f553f45788099da0423b2572c5883719d8044
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 15:56:19 -0700

    Update commLib.js

--- files/mservices/_common/commLib.js
+++ files/mservices/_common/commLib.js
@@ -1,5 +1,8 @@
 var _commLib = function () {
-    this.messageFunction = function(o, fn, func)  {
-        o[fn] = func;
+    this.messageFunction = function(o, fn, data)  {
+        if (typeof data.message === 'function') {
+            o[fn] = data.message;
+        }
+        
     }
 };
