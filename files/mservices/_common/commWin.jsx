52cda085742dc640f617b8802db8caa34646a350
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 13:34:09 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -40,6 +40,9 @@ try {
 			    box_style={}, close_icon = '';
 			// console.log(me.callMessage()); --=={me.callMessage()}==--
 			// close_icon = (me.state.ModalPlus.close_icon === false)?'none':''; me.props.parent[ModalPopup.messageFn]()
+			return (
+				<span>niu123</span>
+				);
 			return (			
 				<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
 					  <div className="modal-dialog modal-lg" role="document">
