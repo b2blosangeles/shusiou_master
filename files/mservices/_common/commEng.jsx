99580d3be0a3cca3b194d3ee6888d8371c6152f8
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 10:43:07 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -173,7 +173,7 @@ try {
 					let eng =  JSON.parse(JSON.stringify(me.props.parent.state._eng));
 					if (!eng.tm) eng.tm = new Date().getTime();
 					let hold = (eng.hold !== null) ? eng.hold : 1000;
-					console.log(eng.tm + '----->' + eng.hold);
+					console.log(eng.tm + '--AA-->' + eng.hold);
 					me._itvEng = setInterval(
 						function() {
 							if (new Date().getTime() - eng.tm > eng.hold) {
