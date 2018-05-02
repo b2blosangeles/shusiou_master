92c4f646dcaea6651950cc14382ac47934448ec2
b2blosangeles <b2blosangeles@gmail.com>
2018-05-02 11:42:42 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -1,59 +1,48 @@
 try {
-	var _WinIndex = 0;
 	var _commWin = React.createClass({		
 		getInitialState: function() {
-			_WinIndex = (!_WinIndex || _WinIndex> 10000) ? 1 : (_WinIndex + 1);
-			return {id : _WinIndex};
+			return {};
 		},
 		componentDidUpdate:function(prePropos, prevStat) {
 			var me = this;
 			if (me.props.parent.state.ModalPopup === 'cancel') {
-				me.props.parent.setState({ModalPopup:null},
-					function() {
-						viewpoint.find('.ModalPopup_' + me.state.id).modal('hide');
-					}
-				);
+				me.props.parent.setState({ModalPopup:null});
 				//me.render();
-				
+				viewpoint.find('.ModalPopup').modal('hide');
 			} else if (me.props.parent.state.ModalPopup) {
 				//me.render();
-				viewpoint.find('.ModalPopup_' + me.state.id).modal({backdrop:'static'});
-			}
+				viewpoint.find('.ModalPopup').modal({backdrop:'static'});
+			}			
 		},
 		closePopup : function() {
 			var me = this;
-			me.props.parent.setState({ModalPopup:'cancel'}, function() {
-				viewpoint.find('.ModalPopup_' + me.state.id).modal('hide');
-			});
+			me.props.parent.setState({ModalPopup:'cancel'});
 		},		
 		ModalLoadingClass: function() {
 			let me = this;	
-			return 'modal fade ModalPopup_' + me.state.id;
+			return 'modal fade ModalPopup';
 		},		
 		render: function() {
-			let me = this, ModalPopup = (me.props.parent.state.ModalPopup) ? me.props.parent.state.ModalPopup : null;
-			if (!ModalPopup || ModalPopup === 'cancel') 
-				return (<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true"/>);
-			else {
-				var box_class = 'danger', 
-				    message = ModalPopup.message, 
-				    box_style={}, close_icon = '';
-				// close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
-				return(
+			let me = this, ModalPopup = (me.props.parent.state.ModalPopup) ? me.props.parent.state.ModalPopup : {};
+			var box_class = 'danger', 
+			    message = ModalPopup.message, 
+			    box_style={}, close_icon = '';
+			// close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
+			return (			
 				<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
-				  <div className="modal-dialog modal-lg" role="document">
-					<div className={'alert alert-' + box_class} style={box_style} role="alert">
-						<span dangerouslySetInnerHTML={{__html: message}}></span>
-						<button type="button" className="close" 
-							onClick={me.closePopup.bind(me)}
-							style={{display:close_icon}}>
-							&times;
-						</button>
-					</div>
-				  </div>
-				</div>		
-				);	
-			}
+					  <div className="modal-dialog modal-lg" role="document">
+						<div className={'alert alert-' + box_class} style={box_style} role="alert">
+							--{ModalPopup.mssageFn}--
+							<span dangerouslySetInnerHTML={{__html: message}}></span>
+							<button type="button" className="close" 
+								onClick={me.closePopup.bind(me)}
+								style={{display:close_icon}}>
+								&times;
+							</button>
+						</div>
+					  </div>
+				</div>	
+			);
 		}		
 		
 	});	
