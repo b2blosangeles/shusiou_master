19913c67d8dd62c42f127232ecab037b7b1e222e
b2blosangeles <b2blosangeles@gmail.com>
2018-05-03 16:50:56 -0700

    Update commLib.js

--- files/mservices/_common/commLib.js
+++ files/mservices/_common/commLib.js
@@ -5,8 +5,15 @@ var _commLib = function () {
             if (typeof data[key] === 'function') {
                 o[ caller + '_' + key] = data[key];
                 delete data[key];
-            }      
+            }
         }
-        
     }
+	this.toHHMMSS = function(v, noms) {
+        if (isNaN(v)) return v;
+        var h = Math.floor(v / 3600),m = ("00" + Math.floor((v % 3600) / 60)).slice(-2),
+                s = ("00" + (Math.floor(v) % 3600) % 60).slice(-2), ms = 1000 * (v - Math.floor(v));
+            if (!noms) { ms = (ms)?'&#189;':''; }
+            else ms = '';
+        return h + ':' + m + ':' + s + ' ' + ms;
+	}
 };
