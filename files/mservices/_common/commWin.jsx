6a5eef9e20724da4b9e173375a484674cb5ed81a
b2blosangeles <b2blosangeles@gmail.com>
2018-05-03 08:54:49 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -60,10 +60,10 @@ try {
 				return (			
 					<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
 						  <div className="modal-dialog modal-lg" role="document">
-							<div className={box_class} style={box_style} role="alert">
+							<div className={box_class} style={{padding:'1em'}} role="alert">
 								<button type="button" className="close" 
 									onClick={me.closePopup.bind(me)}
-									style={{display:close_icon, padding:'0.5em'}}>
+									style={{display:close_icon}}>
 									&times;
 								</button>
 								{me.callMessage()}
