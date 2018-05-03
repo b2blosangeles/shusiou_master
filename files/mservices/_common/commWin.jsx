8efd83e1a7dab0523697197ef732363df44bc349
b2blosangeles <b2blosangeles@gmail.com>
2018-05-03 16:35:44 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -62,15 +62,13 @@ try {
 				return (			
 					<div className={me.ModalLoadingClass()} tabindex="-1" role="dialog" aria-hidden="true">
 						<div className="modal-dialog modal-lg" role="document">
-							<div className={box_class}>
-								<div className="container-fluid" style={{padding:'0.5em'}}>
-									<button type="button" className="close" 
-										onClick={me.closePopup.bind(me)}
-										style={{display:close_icon}}>
-										&times;
-									</button>
-									{me.callMessage()}
-								</div>
+							<div className={box_class + ' container-fluid'} style={{padding:'0.5em'}}>
+								<button type="button" className="close" 
+									onClick={me.closePopup.bind(me)}
+									style={{display:close_icon}}>
+									&times;
+								</button>
+								{me.callMessage()}
 							</div>
 						  </div>
 					</div>	
