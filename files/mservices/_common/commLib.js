1b19d797124ce87118a864f9785cbe23e45284cb
b2blosangeles <b2blosangeles@gmail.com>
2018-05-04 17:12:35 -0700

    Update commLib.js

--- files/mservices/_common/commLib.js
+++ files/mservices/_common/commLib.js
@@ -1,5 +1,5 @@
 var _commLib = function () {
-    alert( arguments.callee.nam);
+    alert( arguments.callee.name);
     this.buildPopup = function(o, data, caller)  {
         for (var key in data) {
             if (typeof data[key] === 'function') {
