50e4c800a1c661411ca70c6c5c314b92b9c40c82
b2blosangeles <b2blosangeles@gmail.com>
2018-05-04 17:04:22 -0700

    Update commLib.js

--- files/mservices/_common/commLib.js
+++ files/mservices/_common/commLib.js
@@ -24,6 +24,7 @@ var _commLib = function () {
         
     }
     this.transferFunction = function(o, data, caller)  {
+        alert(arguments.callee.name)
         for (var key in data) {
             if (typeof data[key] === 'function') {
                 o[ caller + '_' + key] = data[key];
