ca4aaa0d4bb480830266bd37ce2532fde4409158
b2blosangeles <b2blosangeles@gmail.com>
2018-05-05 06:25:40 -0700

    Update embed_video_editor.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_video_editor.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_video_editor.jsx
@@ -110,9 +110,7 @@ try {
 			if (p_video) p_video.pause();
 			v.s = me.state.track.s;
 			v.t = me.state.track.t;
-			// me.props.parent.popupEditVideo_close();		
-			alert(122);
-			me.props.parent.props.parent.closePopup();
+			me.props.parent.popupEditVideo_close();
 		},
 		showSectionImages: function() {
 			var me = this, A = [];
