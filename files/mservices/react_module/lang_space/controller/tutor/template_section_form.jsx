108a49c83310861f0750ef7565d50fe5f4e7198f
b2blosangeles <b2blosangeles@gmail.com>
2018-05-15 17:39:17 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -202,7 +202,7 @@ try {
 									{(function() {
 										return (<span dangerouslySetInnerHTML=
 										{{__html: 'Start: ' + me.props.parent.toHHMMSS(me.state.data[v].s) + 
-										' To:' + me.props.parent.toHHMMSS(me.state.data[v].s + me.state.data[v].t)}}
+										' To:' + me.props.parent.toHHMMSS(parseInt(me.state.data[v].s) + parseInt(me.state.data[v].t))}}
 										/>)
 									})()}
 									<button className="btn btn-info btn-xs" 
