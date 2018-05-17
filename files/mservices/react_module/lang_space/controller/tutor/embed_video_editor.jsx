5c247943299b28605aca952958b336e226d50f2f
b2blosangeles <b2blosangeles@gmail.com>
2018-05-16 17:45:29 -0700

    Update embed_video_editor.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_video_editor.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_video_editor.jsx
@@ -68,10 +68,11 @@ try {
 			return (
 				<table id="video_bar" width="100%" height="16" style={{'border':'1px solid #ddd'}}><tr>
 				{X.map(function(x, idx) {	
-					let s = parseFloat(me.state.track.s),
-					    t = parseFloat(me.state.track.t);
-					if (idx >= Math.round(n * s / video_length ) && 
-						idx < Math.round(n * (s + t) / video_length)) {
+					let s0 = parseFloat(me.state.track.s),
+					    t0 = parseFloat(me.state.track.t);
+						console.log(s0 + '---0---' t0);
+					if (idx >= Math.round(n * s0 / video_length ) && 
+						idx < Math.round(n * (s0 + t0) / video_length)) {
 						return (<td width="1" style={{'background-color':'green'}}></td>)
 					}	
 					for (var j = 0; j < me.sections.length; j++) {
