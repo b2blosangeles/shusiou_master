b7dc14930aff68e0f71b0fa8cf5d41a7cbc74171
b2blosangeles <b2blosangeles@gmail.com>
2018-05-17 12:29:08 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -138,11 +138,12 @@ try {
 			let me = this, 
 			    data = {
 				    curriculum_id : me.props.parent.state.curriculum.curriculum_id,
-				    section_id: me.props.section.section_id, 
+				    section_id: me.props.section_id, 
 				    tpl:me.state.c_tpl, 
 				    data:me.state.data, 
 				    c_section:me.state.c_section
 			    };
+			    console.log(data);
 			me.props.env.engine({
 				url: _master_svr() + '/api/curriculum/myCurriculum.api',
 				method: "POST",
