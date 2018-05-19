ae953f88e35b8dc881f79dc89c46341bc816b6c1
b2blosangeles <b2blosangeles@gmail.com>
2018-05-18 17:20:36 -0700

    Update my_curriculums.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
@@ -13,7 +13,7 @@ try {
 			var me = this;
 			me.setState({_eng:{
 				Q:[
-					{code:'', url : _master_svr() +  '/api/curriculum/myCurriculum.api', method:'post', 
+					{code:'getlist', url : _master_svr() +  '/api/curriculum/myCurriculum.api', method:'post', 
 					 data:{cmd:'getList', auth:me.props.route.env.state.auth}},
 					{code:'Pgetlist', parallel:true, 
 					 	list:[
