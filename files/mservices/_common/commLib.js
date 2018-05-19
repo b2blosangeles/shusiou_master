5ef624dd50e43e960d7abc5668d07cf8a2db777f
b2blosangeles <b2blosangeles@gmail.com>
2018-05-18 23:35:51 -0700

    Update commLib.js

--- files/mservices/_common/commLib.js
+++ files/mservices/_common/commLib.js
@@ -49,13 +49,12 @@ var _commLib = function () {
     this.setCallBack = function(o, target) {
        let me = this;
        me.eng_id = (!me.eng_id || me.eng_id > 1000) ? 1 : (me.eng_id + 1);
-       let func = null, id = me.eng_id;
+       let func = null, id = new Date().getTime + '_' + me.eng_id;
         
        if (typeof o.callBack === 'function') {
            func = o.callBack;
-           delete o.callBack;
            target['EngCbk_' + id] = func;
-           o.callback = 'EngCbk_' + id;
+           o.callBack = 'EngCbk_' + id;
        }
     }
     this.obj2Json = function(o) {
