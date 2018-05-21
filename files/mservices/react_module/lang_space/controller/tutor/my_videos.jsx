e0279b218cb867c1b8208822b68af66fef51ba19
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 11:34:11 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -217,6 +217,7 @@ try {
 
 					<br/><br/><br/><br/>
 					<div className="content_bg opacity_bg"/>
+					{Root.lib.landingModal(me)}
 					{/*<_commWin parent={me} /><_commEng parent={me} />*/}
 				</div>
 			);
