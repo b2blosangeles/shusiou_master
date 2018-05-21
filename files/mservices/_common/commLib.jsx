c87ae542069f15107f3a21ebeb97e31017c6771e
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 08:27:29 -0700

    Update commLib.jsx

--- files/mservices/_common/commLib.jsx
+++ files/mservices/_common/commLib.jsx
@@ -3,7 +3,7 @@ var _commLib = function () {
     _LibIndex = (!_LibIndex || _LibIndex > 1000000) ? 1 : (_LibIndex + 1);
     
     this.landingModal = function(o) {
-	var me = this;
+	var me = this, target = (me.target) ?  me.target : me;
     	return(<span><_commWin parent={me.target} /><_commEng parent={me.target} /></span>)
     }
     this.alert = function(target, message, alert_type,  holdTime)  {
