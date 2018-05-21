b2b09fb89386e6292af16445e14f923655709c40
b2blosangeles <b2blosangeles@gmail.com>
2018-05-20 21:16:58 -0700

    Update my_curriculums.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
@@ -38,7 +38,7 @@ try {
 				callBack: function(data) {
 					if (data.status === 'success') {
 						me.setState({list:data.data}, function() {
-							me.popupMessage();
+							me.popupMessage('test');
 						});
 					}
 				}
