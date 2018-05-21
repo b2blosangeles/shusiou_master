9044351728a129ddc93c7dac81fafedb52d7db1a
b2blosangeles <b2blosangeles@gmail.com>
2018-05-20 22:17:03 -0700

    Update my_curriculums.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
@@ -44,7 +44,7 @@ try {
 					if (data.status === 'success') {
 						me.setState({list:data.data}, function() {});
 					} else {
-						me.popupAlert('API access error!', 'danger', 3000);
+						me.popupAlert('API access error!', 'danger', 8000);
 					}
 				}
 			}
