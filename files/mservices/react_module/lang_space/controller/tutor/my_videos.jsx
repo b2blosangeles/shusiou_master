81e6237bc6c1000658bfebdec0f0ceb4b3f35a19
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 15:08:22 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -174,7 +174,7 @@ try {
 					<br/><br/><br/><br/>
 					<div className="content_bg opacity_bg"/>
 					
-					{Root.lib.landingModal(me)}
+					{Root.lib.landingModal(Root)}
 				</div>
 			);
 		}
