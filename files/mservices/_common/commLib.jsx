3c4b29a1d6bfb01cfeb6e96d7a20c14a9f0ef4be
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 15:24:30 -0700

    Update commLib.jsx

--- files/mservices/_common/commLib.jsx
+++ files/mservices/_common/commLib.jsx
@@ -7,9 +7,9 @@ var _commLib = function () {
     }
     this.alert = function(target, message, alert_type,  holdTime)  {
 	    console.log('====target===');
-	    console.log(target.props.name);
+	    console.log(target.props);
 	    console.log('====Root===');
-	    console.log(Root.props.name);	    
+	    console.log(Root.props);	    
 	var me = this, ta = (target) ? target : Root;
 	let cfg = {
 		section: {
