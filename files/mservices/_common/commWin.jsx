2b61d33538a1507ab3fe82b2ead79d25986ee389
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 20:58:51 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -54,7 +54,7 @@ try {
 								style={{display:close_icon}}>
 								&times;
 							</button>						
-							{me.callSection('message')}				
+							{me.callSection('message')}'--'				
 						</div>
 					</div>
 				);
