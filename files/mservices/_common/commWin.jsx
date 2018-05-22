f5dce4c1e74bfd2c06fed6b7b78e235e3baa9d4f
b2blosangeles <b2blosangeles@gmail.com>
2018-05-21 21:23:29 -0700

    Update commWin.jsx

--- files/mservices/_common/commWin.jsx
+++ files/mservices/_common/commWin.jsx
@@ -30,7 +30,7 @@ try {
 			   ) {
 				return me.props.parent[me.props.parent.state.ModalPopup.section[code]]();
 			} else {
-				return 'tt'
+				return me.props.parent[me.state.ModalPopup.section[code]]();
 			}
 		},
 		ModalLoadingClass: function() {
