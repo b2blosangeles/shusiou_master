e2f0264b4ad0b6794717393ec6fc1c7fe18cee5c
b2blosangeles <b2blosangeles@gmail.com>
2018-05-22 23:06:30 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -208,12 +208,12 @@ try {
 								}
 								return (
 								<span>
-									{(function() {
+									{/*(function() {
 										return (<span dangerouslySetInnerHTML=
 										{{__html: 'Start: ' + Root.lib.toHHMMSS(me.state.data[v].s) + 
 										' To:' + Root.lib.toHHMMSS(parseInt(me.state.data[v].s) + parseInt(me.state.data[v].t))}}
 										/>)
-									})()}
+									})()*/}
 									<button className="btn btn-info btn-xs" 
 										onClick={me.popupEditVideo.bind(me, me.state.data[v])}>
 									<i className="fa fa-scissors" aria-hidden="true"></i> Clip video
