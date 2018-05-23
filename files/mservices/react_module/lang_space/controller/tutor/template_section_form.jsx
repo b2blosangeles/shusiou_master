877f69757f7d18365bca23c45fec021a849115b1
b2blosangeles <b2blosangeles@gmail.com>
2018-05-23 09:17:26 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -159,32 +159,11 @@ try {
 				hold:0,
 				setting: {timeout:3000},
 				callBack: function(data) {
-					Root.lib.alert(me, 'Data update success!', 'success', 1000);
+					Root.lib.alert(me, 'Data successfully saved!', 'success', 1000);
 					me.props.parent.refreshSections();
 				}
 			}
 			Root.lib.loadEng(me, engCfg);
-			/*
-			return true;
-			
-			let me = this, 
-			    data = {
-				    curriculum_id : me.props.parent.state.curriculum.curriculum_id,
-				    section_id: me.props.section_id,
-				    tpl:me.state.c_tpl, 
-				    data:me.state.data
-			    };
-			me.props.env.engine({
-				url: _master_svr() + '/api/curriculum/myCurriculum.api',
-				method: "POST",
-				data: { cmd:opt, data: data,  auth:me.props.env.state.auth}, dataType: "JSON"
-			}, function( result) {
-				me.props.parent.refreshSections();
-			},function( jqXHR, textStatus ) {
-				alert(JSON.stringify('error'));
-				console.log('error');
-			});
-			*/
 		},		
 		templateSelectScript: function() {
 			let me = this, scriptLangs = me.state.scriptLangs, scriptList = me.state.scriptList;
