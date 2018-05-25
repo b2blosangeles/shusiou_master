a54df42a262661fc92d29577ef5d71075a3490d9
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 12:09:56 -0700

    Update public_courses.jsx

--- files/mservices/react_module/lang_space/controller/student/public_courses.jsx
+++ files/mservices/react_module/lang_space/controller/student/public_courses.jsx
@@ -6,7 +6,7 @@ try {
 		},	
 		componentDidMount:function() {
 			var me = this;
-			setTimeout(me.callEng, 100);
+			setTimeout(me.callEng, 1);
 			var str='test1[s]test2';
 			var a = str.split(/\[s\]/i);
 		},
