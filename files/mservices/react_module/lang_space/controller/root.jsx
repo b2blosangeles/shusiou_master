91be5be95906e1d3fe52186d45b615faad1e913e
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 09:04:41 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -132,10 +132,9 @@ try {
 				var me = this;
 				var my_role = ((me.state.userInfo) && (me.state.userInfo.roles)) ? 
 				    me.state.userInfo.roles : [];
-				// my_role = ["learner", "teacher", "advisor"];
-				console.log('====me.state.userInfo.roles====');
-				console.log(me.state.userInfo.roles);
-				//me.context.refresh();
+				my_role = ["learner", "teacher", "advisor"];
+				//console.log('====me.state.userInfo.roles====');
+				//console.log(me.state.userInfo.roles);
 				me.matrix = [
 					{route:'tutor/my_curriculums', role:['teacher'], auth:true, component:My_curriculums},
 					{route:'tutor/my_curriculum/:opt/', role:['teacher'],  auth:true, component:MyCurriculumById},
