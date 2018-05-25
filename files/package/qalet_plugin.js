246efef1de385ae3c41fe5109f9d6a79cc81d69a
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 13:23:59 -0700

    Update qalet_plugin.js

--- files/package/qalet_plugin.js
+++ files/package/qalet_plugin.js
@@ -18,7 +18,6 @@
 			"/mservices/_common/commObj.jsx",
 			"/mservices/_common/commEng.jsx",
 			"/mservices/_common/commWin.jsx",
-			"/mservices/_common/dynamicRouter.jsx",
 
 			"/mservices/react_module/lang_space/controller/shusiou_config.js",			
 			
