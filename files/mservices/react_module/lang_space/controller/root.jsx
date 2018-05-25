0d60abfe3ae390ad02fbea5f1ed98ac851f112a1
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 07:33:22 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -17,7 +17,7 @@ try {
 					roles:_DATA_["/data/user_role.json"],
 					c_lang:(!reactCookie.load('lang'))?'cn':reactCookie.load('lang'),
 					c_role:(!reactCookie.load('role'))?'learner':reactCookie.load('role'),
-				//	auth:(reactCookie.load('auth'))?reactCookie.load('auth'):{},
+					auth:(reactCookie.load('auth'))?reactCookie.load('auth'):{},
 					userInfo: {}
 				};
 			},
