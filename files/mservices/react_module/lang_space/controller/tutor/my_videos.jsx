1ffefd2d57887e31c660b50e72460f8b3debba67
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 13:26:33 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -6,7 +6,7 @@ try {
 		},	
 		componentDidMount:function() {
 			var me = this;
-			me.callEng();
+			setTimeout(me.callEng);
 		},
 		componentDidUpdate:function() {
 			var me = this;	
@@ -169,6 +169,7 @@ try {
 
 					<br/><br/><br/><br/>
 					<div className="content_bg opacity_bg"/>
+					{Root.lib.landingModal(me)}
 				</div>
 			);
 		}
