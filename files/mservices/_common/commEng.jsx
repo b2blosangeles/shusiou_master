edd02de804921afae7809656ee00ec8aaba62f4a
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 10:30:01 -0700

    Update commEng.jsx

--- files/mservices/_common/commEng.jsx
+++ files/mservices/_common/commEng.jsx
@@ -6,7 +6,7 @@ try {
 			_EngIndex = (!_EngIndex || _EngIndex > 10000) ? 1 : (_EngIndex + 1);
 			let me = this;
 			me.lib = new _commLib();
-			return {id:_EngIndex, ModalLoading:{}, _eng:{}};
+			return {id:_EngIndex, ModalLoading:{}, _eng:'cancel'};
 		},		
 		ajax: function(CP, rec, done, error) {
 			let me = this;
