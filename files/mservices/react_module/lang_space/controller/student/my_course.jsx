757848d5a1bcde834266693f189eab1c0c04b32b
b2blosangeles <b2blosangeles@gmail.com>
2018-05-25 15:24:16 -0700

    Update my_course.jsx

--- files/mservices/react_module/lang_space/controller/student/my_course.jsx
+++ files/mservices/react_module/lang_space/controller/student/my_course.jsx
@@ -10,7 +10,7 @@ try {
 			
 			var str='test1[s]test2';
 			var a = str.split(/\[s\]/i);
-			console.log(a);
+			//console.log(a);
 			/*
 			$('.video_ad').attr('autoplay', true);
 			$(".video_ad").attr("src", "http://virtual_language_lab.qalet.com/api/lang_space/vr.js?video=sample.mp4");
