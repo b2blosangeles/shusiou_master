2dc2d27d390f1daa41b35c0dd7528b293d94b490
b2blosangeles <b2blosangeles@gmail.com>
2018-05-26 09:23:48 -0700

    Update my_curriculum.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
@@ -77,6 +77,7 @@ try {
 			var vid = '';	
 			if (me.props.params['opt'] == 'new') {
 				vid = me.props.params['id'];
+				/*
 				me.getVideoInfo(vid,
 					function(data) {
 						me.setState({vid:vid, video:data.data[0]});
@@ -84,6 +85,7 @@ try {
 						me.rightBox(me.props.params);				
 					}
 				);
+				*/
 			} else if (me.props.params['opt'] == 'edit') {
 				var cid = me.props.params['id'];
 				me.getCurriculumById(cid, function(data) {		
