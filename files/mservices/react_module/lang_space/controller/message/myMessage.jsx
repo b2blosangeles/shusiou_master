4cd4f873c0ea763e67b4115fdd43c19472c7bb6d
b2blosangeles <b2blosangeles@gmail.com>
2018-05-27 08:44:14 -0700

    Update myMessage.jsx

--- files/mservices/react_module/lang_space/controller/message/myMessage.jsx
+++ files/mservices/react_module/lang_space/controller/message/myMessage.jsx
@@ -6,13 +6,15 @@ try {
         },
 	componentDidMount:function() {
 		var me = this;
-		localStorage.setItem('upload', new Date());
+		localStorage.setItem('upload1', new Date());
+		localStorage.setItem('upload2', new Date());
 	},
         render: function() {
             var me = this;
             return (<div className="content_section">
 			<br/><br/><br/>
-			    {localStorage.getItem('upload')}
+			{localStorage.getItem('upload1')} <br/>
+			{localStorage.getItem('upload2')} <br/>
 		    	<div className="content_bg opacity_bg"/>
 		    </div>)
 	}	
