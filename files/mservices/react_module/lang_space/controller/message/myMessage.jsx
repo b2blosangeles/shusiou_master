433368fc1ce1002b4ea82abf029f27ee62774f69
b2blosangeles <b2blosangeles@gmail.com>
2018-05-27 08:03:51 -0700

    Create myMessage.jsx

---
+++ files/mservices/react_module/lang_space/controller/message/myMessage.jsx
@@ -0,0 +1,15 @@
+try {	
+    var My_message =  React.createClass({
+        getInitialState: function() {
+            var me = this;
+            return {};
+        },		
+        render: function() {
+            var me = this;
+            return (<span></span>
+        );
+    });	
+	
+} catch (err) {
+	  console.log(err.message);
+}
