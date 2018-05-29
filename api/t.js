06a8e29a46104aa18aa222202539b0716ddf3335
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 12:17:13 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,2 +1,3 @@
-var io_client = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
+var io = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
+let ioClient = io.connect("http://localhost:8000");
 res.send(env);
