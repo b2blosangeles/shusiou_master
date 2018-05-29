4813efaa5d5b03af8cab347ed9c4ad9e85e06889
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 12:24:53 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,3 +1,8 @@
 var io = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
-let ioClient = io.connect("http://localhost:8000");
+let ioClient = io.connect("https://dev.shusiou.win/");
+
+ioClient.emit('createRoom', 'VID_NIU'); 
+
+ ioClient.emit('clientData', {room: 'VID_NIU', data: 'niu BB'});
+
 res.send(env);
