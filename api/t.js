094dba3fb4b81b07788d3d76983c16f38eec7a1b
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 12:37:56 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,11 +1,12 @@
-var io = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
+// var io = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
 
 //let ioClient = io.connect("https://dev.shusiou.win/");
 //ioClient.emit('createRoom', 'VID_NIU'); 
 
 // ioClient.emit('clientData', {room: 'VID_NIU', data: 'niu BB'});
 
-var socket = io("https://dev.shusiou.win/");
+// var socket = io("https://dev.shusiou.win/");
+var socket = require(env.site_path + '/api/inc/socket.io-client/node_modules/socket.io-client')('https://dev.shusiou.win');
 socket.on('connect', function(){
   res.send('env--A');
 });
