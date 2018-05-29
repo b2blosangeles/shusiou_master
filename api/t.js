650a46ccf902f0b426103a6454bd41e8759af29b
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 12:50:27 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -6,7 +6,7 @@
 // ioClient.emit('clientData', {room: 'VID_NIU', data: 'niu BB'});
 
 // var socket = io("https://dev.shusiou.win/");
-var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client')('https://dev.shusiou.win');
+var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
 let socket = io.connect("https://dev.shusiou.win/");
 socket.on('connect', function(err){
     console.log('---1---');
