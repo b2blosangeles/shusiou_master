78e0f77acddc99081e4c7c08c10aec5c7dd4aa8b
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 07:22:44 -0700

    Update shusiou_socket.js

--- files/js/shusiou_socket.js
+++ files/js/shusiou_socket.js
@@ -3,8 +3,3 @@ socket.emit('createRoom', 'testroom');
 socket.on('serverData', function(data) {
   console.log(data);
 });
-setTimeout(
-  function() {
-    socket.emit('clientData', {room: 'testroom' , data: {id:'niu', data:'dcsf.sdf', func:'func name'}});
-  }, 3000
-);
