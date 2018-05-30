8be629242686056abcf7de742be6d709a09561a3
b2blosangeles <b2blosangeles@gmail.com>
2018-05-30 09:44:37 -0700

    Update socketNodeClient.js

--- api/inc/socketNodeClient/socketNodeClient.js
+++ api/inc/socketNodeClient/socketNodeClient.js
@@ -23,7 +23,7 @@
 			me.socket.on('serverData', function(data) {
 				if ((data.data) && data.data.requestID === me.requestID) {
 					me.socket.disconnect();
-					callback('- requestID -' + data.data.requestID + ' - >> -' + me.socket.rooms.indexOf(room));
+					callback('- requestID -' + data.data.requestID);
 					
 					return true;
 				}
