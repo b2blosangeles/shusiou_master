72a0a576a35f1403d367952f6e3377fab89f6413
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 21:03:13 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,8 +1,9 @@
 var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
+let requestID = 'asedk';
 socket.on('connect', function(){
     socket.emit('createRoom', 'VID_NIU');
-    socket.emit('clientData', {room: 'VID_NIU', data: 'reloadvideos'});
+    socket.emit('clientData', {room: 'VID_NIU', requestID:requestID, data: requestID});
 
 });
 socket.on('serverData', (function(res) {
