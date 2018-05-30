72d8699c07c1eb852f54273319be8031bcceab86
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 20:49:32 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,5 +1,3 @@
-res.send('data');
-return true;
 var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
 socket.on('connect', function(){
