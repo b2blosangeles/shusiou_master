187260c915e1280ec5e4127aecca7ae310bc36f9
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 21:31:09 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -2,9 +2,11 @@ var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
 let room = 'VID_NIU', requestID = room + '_' + new Date().getTime();
 socket.on('connect', function(){
+
+});
     socket.emit('createRoom', room);
     socket.emit('clientData', {room: room, data: { requestID:requestID, data: 'requestID'}});
-});
+
 socket.on('serverData', (function(res, requestID) {
     return function(data) {
         if (data.requestID === requestID) {
