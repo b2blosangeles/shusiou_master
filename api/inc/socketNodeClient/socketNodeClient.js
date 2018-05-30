11ca7ca0eb9e7ce32b5458b6aacf706aa367df42
b2blosangeles <b2blosangeles@gmail.com>
2018-05-30 08:12:02 -0700

    Update socketNodeClient.js

--- api/inc/socketNodeClient/socketNodeClient.js
+++ api/inc/socketNodeClient/socketNodeClient.js
@@ -2,24 +2,24 @@
 	var obj =  function () {
 		let me = this;
 		me.io = require('../socket.io-client/node_modules/socket.io-client');
-	};
-	sendToRoom = function (room, url, data, callback) {
-		let me = this;
-		me.socket = me.io.connect(url, {secure: true, reconnect: true, rejectUnauthorized : false});
-		me.requestID = room + '_' + new Date().getTime();
-		
-		me.socket.on('connect', function(){
-		    me.socket.emit('createRoom', room);
-		    setTimeout(function() {
-			me.socket.emit('clientData', {room: room, data: { requestID:me.requestID, data: data}});
-		    });
-		});
-		me.socket.on('serverData', function(data) {
-				if ((data.data) && data.data.requestID === me.requestID) {
-					me.socket.disconnect();
-					callback(me.requestID + '===---' + data.data.requestID);
-				}
-			});		
-	};
+		this.sendToRoom = function (room, url, data, callback) {
+			let me = this;
+			me.socket = me.io.connect(url, {secure: true, reconnect: true, rejectUnauthorized : false});
+			me.requestID = room + '_' + new Date().getTime();
+
+			me.socket.on('connect', function(){
+			    me.socket.emit('createRoom', room);
+			    setTimeout(function() {
+				me.socket.emit('clientData', {room: room, data: { requestID:me.requestID, data: data}});
+			    });
+			});
+			me.socket.on('serverData', function(data) {
+					if ((data.data) && data.data.requestID === me.requestID) {
+						me.socket.disconnect();
+						callback(me.requestID + '===---' + data.data.requestID);
+					}
+				});		
+		};
+	}	
 	module.exports = obj;
 })();
