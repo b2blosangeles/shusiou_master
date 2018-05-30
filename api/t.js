1cf075a77ce8abc3817c25daf07538a91cbea525
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 18:02:24 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -2,7 +2,7 @@ var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
 socket.on('connect', function(err, a){
     socket.emit('createRoom', 'VID_NIU');
-    Root.socket.emit('clientData', {room: 'VID_NIU', data: 'reloadvideos'});
+    socket.emit('clientData', {room: 'VID_NIU', data: 'reloadvideos'});
     socket.on('serverMessage', function(data) {
          console.log('---2---');
         console.log(data);
