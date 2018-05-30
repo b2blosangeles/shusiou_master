2adec6be722b334c42e1479fad1b4301a54c6d76
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 21:07:23 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -7,6 +7,7 @@ socket.on('connect', function(){
 
 });
 socket.on('serverData', (function(res) {
+    return function(data) {
         socket.disconnect();
         res.send('data_'+requestID);
     }  
