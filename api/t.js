3cdccd47c62323ba29c9314d7c9ad60d003d6122
b2blosangeles <b2blosangeles@gmail.com>
2018-05-30 08:26:27 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,7 +1,7 @@
 let socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
 let socketClient = new socketNodeClient();
 
-socketClient.sendToRoom('VID_NIU', 'https://dev.shusiou.win/', {secure: true, reconnect: true, rejectUnauthorized : false},
+socketNodeClient.sendToRoom('VID_NIU', 'https://dev.shusiou.win/', {secure: true, reconnect: true, rejectUnauthorized : false},
     function(data) {
         res.send(data);
     }
