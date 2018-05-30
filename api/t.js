ea77ed54ed6c0b0101d4292a2c030df16a7f5319
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 20:34:56 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,6 +1,6 @@
 var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
-socket.on('connect', function(err, a){
+socket.on('connect', function(){
     socket.emit('createRoom', 'VID_NIU');
     socket.emit('clientData', {room: 'VID_NIU', data: 'reloadvideos'});
         res.send(true);
