9af2f44ec27a175b9cc66fe36fbf80d7b3138b7f
b2blosangeles <b2blosangeles@gmail.com>
2018-05-30 09:53:36 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,12 +1,8 @@
-
-var config = require(env.config_path + '/config.json');
-res.send('https://' + config.root + '/');
-return true;
-
 delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
 let socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
 
-let socketClient = new socketNodeClient('https://dev.shusiou.win/');
+var config = require(env.config_path + '/config.json');
+let socketClient = new socketNodeClient('https://' + config.root + '/');
 
 socketClient.sendToRoom(
     'VID_NIU',
@@ -15,26 +11,3 @@ socketClient.sendToRoom(
         res.send(data);
     }
 );
-
-return true;
-/*
-var io = require(env.sites_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
-let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
-let room = 'VID_NIU', requestID = room + '_' + new Date().getTime();
-
-socket.on('connect', function(){
-    socket.emit('createRoom', room);
-    setTimeout(function() {
-        socket.emit('clientData', {room: room, data: { requestID:requestID, data: 'requestID'}});
-    });
-});
-    
-    socket.on('serverData', (function(res, requestID) {
-        return function(data) {
-            if ((data.data) && data.data.requestID === requestID) {
-                socket.disconnect();
-                res.send(requestID + '---' + data.data.requestID);
-            }
-        }  
-    })(res, requestID));
-*/
