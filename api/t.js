dce7cfa1e2a4116dbdd12f13a27dac2dad5edb27
b2blosangeles <b2blosangeles@gmail.com>
2018-05-29 22:06:30 -0700

    Update t.js

--- api/t.js
+++ api/t.js
@@ -1,4 +1,4 @@
-var io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
+var io = require(env.sites_path + '/api/inc/socket.io-client/node_modules/socket.io-client');
 let socket = io.connect("https://dev.shusiou.win/", {secure: true, reconnect: true, rejectUnauthorized : false});
 let room = 'VID_NIU', requestID = room + '_' + new Date().getTime();
 
