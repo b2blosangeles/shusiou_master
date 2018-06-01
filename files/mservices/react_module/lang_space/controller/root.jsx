2ec38453df9a676c3138f029927be34a9485562c
b2blosangeles <b2blosangeles@gmail.com>
2018-06-01 13:25:14 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -47,11 +47,12 @@ try {
 						obj.socket.emit('createRoom', cfg.room);
 						if (typeof cfg.onServerData === 'function') {
 							obj.socket.on('serverData', (function(socket) {
-								return function(incomeData) {
-									console.log('====>>' + socket.id);
-									if (incomeData._room === cfg.room) {
-										cfg.onServerData(incomeData);
-									}
+									return function(incomeData) {
+										console.log('====>>' + socket.id);
+										if (incomeData._room === cfg.room) {
+											cfg.onServerData(incomeData);
+										}
+									}	
 								})(obj.socket)
 							});
 						}	
