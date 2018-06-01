454b5f9eb981a1bb2ac84bffe87961f9bf2ae415
b2blosangeles <b2blosangeles@gmail.com>
2018-06-01 15:35:26 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -21,9 +21,9 @@ try {
 			},
 			loadSocketIO : function(o, cfg) {
 				var me = this;
-				console.log(o.props.name);
+				console.log(o.location);
 				Root._ioid = (!Root._ioid || Root._ioid > 999999) ? 1 : ++Root._ioid;
-				let _id = 'IO_' + Root._ioid;
+				let _id = cfg.room + '_' + cfg.id;
 				console.log('_id =D=>' + _id);
 				Root.socket = (Root.socket) ? Root.socket : {};
 				Root.socket[_id] = (Root.socket[_id]) ? Root.socket[_id] : {};
