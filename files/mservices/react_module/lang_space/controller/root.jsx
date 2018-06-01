fd8057988439ca228da37cbac9ca24205863786d
b2blosangeles <b2blosangeles@gmail.com>
2018-06-01 15:51:17 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -23,7 +23,9 @@ try {
 				var me = this;
 				let _id = (cfg.id) ? (cfg.id + '_' + cfg.room) :
 				    (!o || !o.props || !o.props.route || !o.props.route.path) ? cfg.room : (o.props.route.path + '_' + cfg.room);
-				console.log('_id =D=>' + _id);
+				
+				// console.log('_id =D=>' + _id);
+				
 				Root.socket = (Root.socket) ? Root.socket : {};
 				Root.socket[_id] = (Root.socket[_id]) ? Root.socket[_id] : {};
 				
@@ -35,23 +37,23 @@ try {
 							if (typeof componentWillUnmount === 'function') {
 								componentWillUnmount();
 							}
-							console.log('---componentWillUnmount triggled ==' + _id);
+							// console.log('---componentWillUnmount triggled ==' + _id);
 							o.socket.close();
 						}
 					})(o, o.componentWillUnmount);
 				}
 				if (!cfg.public && (obj.socket)) {
-					console.log('o.socket.close();');
+					// console.log('o.socket.close();');
 					obj.socket.close();
 				}
 				if (!obj.socket) {
 					obj.socket = io.connect(cfg.resource);
 					obj.socket.on('connect', function() {
-						console.log('--->connected -->' + obj.socket.id);
+						// console.log('--->connected -->' + obj.socket.id);
 						obj.socket.emit('createRoom', cfg.room);
 						if (typeof cfg.onServerData === 'function') {
 							obj.socket.on('serverData', function(incomeData) {
-								console.log('====>>' + obj.socket.id);
+								// console.log('====>>' + obj.socket.id);
 								if (incomeData._room === cfg.room) {
 									cfg.onServerData(incomeData, obj.socket);
 								}
