2167bde2cbd2151844d9cd1d0338405b85cf3af1
b2blosangeles <b2blosangeles@gmail.com>
2018-06-01 10:10:57 -0700

    Update root.jsx

--- files/mservices/react_module/lang_space/controller/root.jsx
+++ files/mservices/react_module/lang_space/controller/root.jsx
@@ -20,7 +20,8 @@ try {
 				};
 			},
 			loadSocketIO : function(o, cfg) {
-				let global = (o) ? o : Root.socket; 
+				Root.socket = (Root.socket) ? Root.socket : {};
+				let obj = (!cfg.public) ? o : Root.socket[cfg.resource]; 
 				o.componentWillUnmount = function() {
 					console.log('---componentWillUnmount triggled');
 					this.socket.close();
@@ -29,20 +30,22 @@ try {
 					console.log('o.socket.close();');
 					o.socket.close();
 				}
-				o.socket = io.connect(cfg.resource);
-				o.socket.on('connect', function() {
-					console.log('--->connected -->' + o.socket.id);
-					o.socket.emit('createRoom', cfg.room);
-					if (typeof cfg.onServerData === 'function') {
-						o.socket.on('serverData', function(incomeData) {
-							if (incomeData._room === cfg.room) {
-								cfg.onServerData(incomeData);
-							}
-						});
-					}	
-				});
-				if (typeof cfg.onServerMessage === 'function') {
-					o.socket.on('serverData', cfg.onServerMessage);
+				if (!o.socke) {
+					o.socket = io.connect(cfg.resource);
+					o.socket.on('connect', function() {
+						console.log('--->connected -->' + o.socket.id);
+						o.socket.emit('createRoom', cfg.room);
+						if (typeof cfg.onServerData === 'function') {
+							o.socket.on('serverData', function(incomeData) {
+								if (incomeData._room === cfg.room) {
+									cfg.onServerData(incomeData);
+								}
+							});
+						}	
+					});
+					if (typeof cfg.onServerMessage === 'function') {
+						o.socket.on('serverData', cfg.onServerMessage);
+					}
 				}
 			},
 			dictionary: function(v) {
