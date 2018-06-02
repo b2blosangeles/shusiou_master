5545712cc65d0c7e5c20656191ceff84eaf58a7c
b2blosangeles <b2blosangeles@gmail.com>
2018-06-01 22:30:11 -0700

    Update socketNodeClient.js

--- api/inc/socketNodeClient/socketNodeClient.js
+++ api/inc/socketNodeClient/socketNodeClient.js
@@ -1,7 +1,7 @@
 (function () { 
 	var obj =  function (url) {
 		let me = this;
-		me.io = require('/var/qalet/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
+		me.io = require(env.root_path + '/sites/master/api/inc/socket.io-client/node_modules/socket.io-client');
 
 		me.connect = function () {
 			let me = this;
