5e56b04a9c29fa1ee6a2dcf8b5cd71f108988e78
b2blosangeles <b2blosangeles@gmail.com>
2018-06-03 07:51:16 -0700

    Update awsS3VideoAdmin.js

--- api/inc/awsS3Video/awsS3VideoAdmin.js
+++ api/inc/awsS3Video/awsS3VideoAdmin.js
@@ -83,7 +83,7 @@
 			let me = this, remove_list = [];
 			var connection = pkg.mysql.createConnection(config.db);
 			connection.connect();
-			var str = 'SELECT `vid` FRom `video` WHERE IN (' + list.join(',') + ')';
+			var str = 'SELECT `vid` FRom `video` WHERE `vid` IN (' + list.join(',') + ')';
 
 			connection.query(str, function (err, results, fields) {
 				connection.end();
