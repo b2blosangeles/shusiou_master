3708d6b16847d32be98bc3a1d6f589814165565d
b2blosangeles <b2blosangeles@gmail.com>
2018-06-02 21:52:37 -0700

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -6,6 +6,15 @@ env.config_path = '/var/qalet_config';
 
 var config = require(env.config_path + '/config.json');
 
+
+/* --- code for cron watch ---*/
+delete require.cache[__dirname + '/watch_cron.inc.js'];
+let watch_cron_inc = require(__dirname + '/watch_cron.inc.js'),
+    watchCron = new watch_cron_inc(__filename);
+watchCron.load('master', 60);
+/* --- code for cron watch  ---*/
+
+
 /* -------------*/
 delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
 var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
@@ -20,6 +29,20 @@ socketClient.sendToRoom(
 );
 /* ------------- */
 
+var sendToFrontendNotice = function(vid, cbk) {
+	var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
+	var socketClient = new socketNodeClient('https://' + config.root + '/', env);
+	// console.log('send message to video_' +  vid);
+	socketClient.sendToRoom(
+	    'video_' +  vid,
+	    {reload:true},
+	    function(data) {
+		cbk(true);
+	    }
+	);			
+};
+
+
 let pkg = {
     	mysql		: require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
     	crowdProcess	: require(env.root_path + '/package/crowdProcess/crowdProcess'),
@@ -31,63 +54,34 @@ let pkg = {
 let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js');
 let tm = new Date().getTime();
 
+var IN = [];
 function s() {
-	return true;
 	let delta_time0 = new Date().getTime() - tm;
-	console.log('---- load at ----> ' +  delta_time0);
-	var splitVideo = new awsS3Video(config, env, pkg, tm);	
+	// console.log('---- load at ----> ' +  delta_time0);
+	var splitVideo = new awsS3Video(IN, config, env, pkg, tm);	
 	splitVideo.load(function(data) {
 		let delta_time = new Date().getTime() - tm;
-		console.log(data);
-		if (delta_time < 50000 && data !== 'No new id at all') {
-				//delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
-				//var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
-				// var socketClient = new socketNodeClient('https://' + config.root + '/');
-				/*
-				socketClient.sendToRoom(
-				    'VID_NIU',
-				    {x:new Date(), Y:6},
-				    function(data) {
-					// res.send(data);
-				    }
-				);*/			
+		if (delta_time < 40000 && data !== 'No new id at all') {
 			s();
 		} else {
-			console.log('---- stopped ----> ');
-				//delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
-				//var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
-				//var socketClient = new socketNodeClient('https://' + config.root + '/');
-				/*
-				socketClient.sendToRoom(
-				    'VID_NIU',
-				    {x:new Date(), Y:1},
-				    function(data) {
-					// res.send(data);
-				    }
-				);
-				*/
-			process.exit(-1);
-			return true;
+			//console.log('*** -IN- ***>');
+			//console.log(IN);
+			if (IN.length) {
+				sendToFrontendNotice(IN[0], function(data) {
+					console.log('====this.sendToFrontendNotice(vid)===>' + IN[0]);
+					console.log('exit current session');
+					process.exit(-1);
+					return true;
+				});
+			} else {
+				console.log('exit current session');
+				process.exit(-1);
+				return true;
+			}
+			
+			
 		}
 		
 	});
 }
 s();
-
-//delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
-//var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
-//var socketClient = new socketNodeClient('https://' + config.root + '/');
-/*
-socketClient.sendToRoom(
-    'VID_NIU',
-    {x:new Date()},
-    function(data) {
-      //  res.send(data);
-    }
-);
-*/
-/* --- code for cron watch ---*/
-delete require.cache[__dirname + '/watch_cron.inc.js'];
-let watch_cron_inc = require(__dirname + '/watch_cron.inc.js'),
-    watchCron = new watch_cron_inc(__filename);
-watchCron.load('master', 60);
