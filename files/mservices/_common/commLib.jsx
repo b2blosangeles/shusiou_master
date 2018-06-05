b0e7c210dac449d50e0c569f8feb33cd36e17215
b2blosangeles <b2blosangeles@gmail.com>
2018-06-04 21:29:09 -0700

    Update commLib.jsx

--- files/mservices/_common/commLib.jsx
+++ files/mservices/_common/commLib.jsx
@@ -54,9 +54,9 @@ var _commLib = function () {
        if (typeof engCfg.callBack === 'function') {
            func = engCfg.callBack;
 	   ta['EngCbk_' + id] = function(data) {
-		   console.log('===data===>');
+		   console.log('===data--->');
 		   console.log(data);
-             //  let me = target;
+                let me = target;
 		delete Root._EngC;
                func(data);
                delete ta['EngCbk_' + id];
