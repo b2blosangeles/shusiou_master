12471852952260080d9f4af611ebeab7729d42ff
b2blosangeles <b2blosangeles@gmail.com>
2018-06-05 14:18:10 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -150,7 +150,7 @@ try {
 						<div className="col-sm-4 col-lg-4 col-md-4"> 
 							<div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
 								<div className="video_thumbnail_icon_group">
-									<button type="button" className="btn btn-danger"
+									<button type="button" className="btn btn-success"
 										onClick={me.videoAdmin.bind(me,'admin')}>
 										<i className="fa fa-play" aria-hidden="true"></i> 
 										&nbsp;{Root.lib.dictionary('add_video')}
@@ -165,7 +165,7 @@ try {
 	
 								<div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
 									<div className="video_thumbnail_icon_group">
-										<button type="button" className="btn btn-success"
+										<button type="button" className="btn btn-warning"
 											onClick={me.videoInfo.bind(me,a)}>
 											<i className="fa fa-play" aria-hidden="true"></i> 
 											&nbsp;{Root.lib.dictionary('play_video')}
