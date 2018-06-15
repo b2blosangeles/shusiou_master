caf792e83d8554e63544801dd4848d9df9bb816d
b2blosangeles <b2blosangeles@gmail.com>
2018-06-15 09:49:18 -0700

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -136,7 +136,7 @@ try {
 						    <tr>
 						      <th scope="col"></th>
 						      <th scope="col">Answer</th>
-						      <th scope="col"><i className="fa fa-plus-square fa-2"></i></th>
+						      <th scope="col"><i className="fa fa-plus-square fa-3"></i></th>
 						    </tr>
 						  </thead>
 						  <tbody>
@@ -146,7 +146,7 @@ try {
 							placeholder={'input text ' + idx} 
 							value={me.state.data[idx]}  
 							onChange={me.handleChange.bind(me, idx)}/></td>
-						      <td> <i className="fa fa-trash fa-2"></i></td>
+						      <td> <i className="fa fa-trash fa-3"></i></td>
 						    </tr>
 						  </tbody>
 						</table>							
