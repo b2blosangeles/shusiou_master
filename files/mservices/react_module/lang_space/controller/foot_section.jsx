41bd73a9c6f79c3dff19a1b477f3457ea76eef57
b2blosangeles <b2blosangeles@gmail.com>
2018-06-17 05:57:55 -0700

    Update foot_section.jsx

--- files/mservices/react_module/lang_space/controller/foot_section.jsx
+++ files/mservices/react_module/lang_space/controller/foot_section.jsx
@@ -133,9 +133,7 @@ try {
 							<span>&copy;  {me.dictionary('virtual_language_lab')} {new Date().getFullYear()}</span>
 							<span>&nbsp;&nbsp;&nbsp;<a href="#/Doc/Privacy">{me.dictionary('menu_privacy')}</a></span>
 							<span>&nbsp;&nbsp;<a href="#/Doc/Terms">{me.dictionary('menu_terms')}</a></span>
-							<span>&nbsp;&nbsp;<a href="https://dev.shusiou.win/voice.html" 
-							onclick={me.channel.bind(me)}>加密通信</a>
-							</span>
+							<span>&nbsp;&nbsp;<input type="button" value="加密通信" onclick={me.channel.bind(me)}/></span>
 						</span>
 					</div>
 					<div className="row"></div>					
