db24cccaf21d59d5ddf4d11fc80e881aa3d9343a
b2blosangeles <b2blosangeles@gmail.com>
2018-06-18 09:47:20 -0700

    Update cron_whoami.js

--- cron_service/cron_whoami.js
+++ cron_service/cron_whoami.js
@@ -5,7 +5,7 @@ env.config_path = '/var/qalet_config';
 var config = require(env.config_path + '/config.json');
 
 /* ----- test code --------*/
-/*
+
 delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
 var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
 var socketClient = new socketNodeClient('https://' + config.root + '/', env);
@@ -17,7 +17,7 @@ socketClient.sendToRoom(
 	// res.send(data);
     }
 );
-*/
+
 /* -------------*/
 
 let pkg = {
