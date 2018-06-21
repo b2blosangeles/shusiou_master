ffcd9c9ba31a5bcf3cec71453c776317fd96d42f
b2blosangeles <b2blosangeles@gmail.com>
2018-06-21 16:53:06 -0700

    Update commLib.jsx

--- files/mservices/_common/commLib.jsx
+++ files/mservices/_common/commLib.jsx
@@ -188,9 +188,7 @@ var _commLib = function () {
 				if (cfg.room) obj.socket.emit('createRoom', cfg.room);
 				if (typeof cfg.onServerData === 'function') {
 					obj.socket.on('serverData', function(incomeData) {
-						if (incomeData._room === cfg.room) {
-							cfg.onServerData(incomeData, obj.socket);
-						}
+						cfg.onServerData(incomeData, obj.socket);
 					});
 				}	
 			});
