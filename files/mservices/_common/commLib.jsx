1c6a8d8dc3020378627e39191e2dcd613d6179e6
b2blosangeles <b2blosangeles@gmail.com>
2018-06-21 16:51:19 -0700

    Update commLib.jsx

--- files/mservices/_common/commLib.jsx
+++ files/mservices/_common/commLib.jsx
@@ -185,8 +185,7 @@ var _commLib = function () {
 		if (!obj.socket) {
 			obj.socket = io.connect(cfg.resource);
 			obj.socket.on('connect', function() {
-				// console.log('--->connected -->' + obj.socket.id);
-				obj.socket.emit('createRoom', cfg.room);
+				if (cfg.room) obj.socket.emit('createRoom', cfg.room);
 				if (typeof cfg.onServerData === 'function') {
 					obj.socket.on('serverData', function(incomeData) {
 						if (incomeData._room === cfg.room) {
