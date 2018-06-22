4a2000b6db9e5afad12d10fa468909b4ef139bdf
b2blosangeles <b2blosangeles@gmail.com>
2018-06-21 23:34:57 -0700

    Update cron_whoami.js

--- cron_service/cron_whoami.js
+++ cron_service/cron_whoami.js
@@ -11,7 +11,10 @@ var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socket
 // var socketClient = new socketNodeClient('https://' + config.root + '/', env);
 
 var socketClient = new socketNodeClient(
-	{link:'http://comm1.service.dev.shusiou.win/'}, 
+	{
+		link:'http://comm1.service.dev.shusiou.win/',
+		proxy:['http://comm1.service.dev.shusiou.win/', 'https://comm1.service.dev.shusiou.win/']
+	}, 
 	env);
 
 socketClient.sendToRoom(
