ecbeef83d4abfb9f765dd357ce5c9e4642073ec1
b2blosangeles <b2blosangeles@gmail.com>
2018-06-22 08:43:42 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -41,7 +41,7 @@ try {
 				console.log('onServerData -- ' + ' === ' + socket.id);
 			},
 			onConnection : function(socket) {
-				console.log('load onConnection --' + socket.id );
+				console.log('load onConnection -gg-' + socket.id );
 			}
 			/*,
 			onServerMessage: function(data) {
