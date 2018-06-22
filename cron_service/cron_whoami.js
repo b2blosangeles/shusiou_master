8b8ece07d93a1764e0812afdcd08264f832ede9a
b2blosangeles <b2blosangeles@gmail.com>
2018-06-21 23:35:36 -0700

    Update cron_whoami.js

--- cron_service/cron_whoami.js
+++ cron_service/cron_whoami.js
@@ -8,7 +8,6 @@ var config = require(env.config_path + '/config.json');
 
 delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
 var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
-// var socketClient = new socketNodeClient('https://' + config.root + '/', env);
 
 var socketClient = new socketNodeClient(
 	{
