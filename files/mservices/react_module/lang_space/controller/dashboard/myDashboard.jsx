85c3ea43ad36c9314c9aef55de1a23c637c20c2d
b2blosangeles <b2blosangeles@gmail.com>
2018-06-23 22:59:46 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -31,7 +31,7 @@ try {
 		console.log('socket.id--' + socket.id);
 		//setInterval(function() {
 			// socket.emit('clientData', {_room: '/#xBmfsjCedzcNQEaCAADQ', 
-			socket.emit('clientData', {_socket: 'BmfsjCedzcNQEaCAADQ',
+			socket.emit('clientData', {_socket: 'xBmfsjCedzcNQEaCAADQ',
 				_link: _link, _proxy: _proxy, 
 				data: {command: 'stop audio', sender:socket.id}},
 				   function(error, message){
