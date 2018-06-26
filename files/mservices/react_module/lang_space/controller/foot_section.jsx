6a659314c5c816bd2e136994b889310663bcc2a4
b2blosangeles <b2blosangeles@gmail.com>
2018-06-25 21:46:14 -0700

    Update foot_section.jsx

--- files/mservices/react_module/lang_space/controller/foot_section.jsx
+++ files/mservices/react_module/lang_space/controller/foot_section.jsx
@@ -117,6 +117,7 @@ try {
 			}
 		},
 		channel : function() {
+			if (!Root.audio_socket) return true;
 			// let url = "https://comm1.service.dev.shusiou.win/?room=CRON_REPORT_A";
 			let url = 'https://comm1.service.dev.shusiou.win/?socket=' + Root.audio_socket
 			positionedPopup(url, 'myWindow','700','300','400','400','yes');
