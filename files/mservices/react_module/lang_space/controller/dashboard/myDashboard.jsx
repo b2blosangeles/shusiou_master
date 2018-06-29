7ed9e424f7e6412dd1313c5f10b6a8b595a2d71f
b2blosangeles <b2blosangeles@gmail.com>
2018-06-28 22:02:34 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -53,7 +53,11 @@ try {
 					master_socket_id: null, 
 					link : 'https://comm1.service.dev.shusiou.win/', 
 					proxy: ['http://comm1.service.dev.shusiou.win/', 
-						'https://comm1.service.dev.shusiou.win/']
+						'https://comm1.service.dev.shusiou.win/'],
+					onConnect : function(socket) {
+						console.log(socket.id);
+						Root.audio_socket = socket.id;
+					}
 				});				
 			}, 1000
 		);
