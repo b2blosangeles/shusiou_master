bde495c6cf707c2c597955be9b5f175adb5d19d6
b2blosangeles <b2blosangeles@gmail.com>
2018-06-29 15:27:23 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -62,7 +62,7 @@ try {
 					onServerData : function(incomeData, socket) {
 						console.log('customized onServerData ');
 						console.log(incomeData);
-						console.log(qna_server.getClients());
+						console.log(me.qna_server.getClients());
 						me.qna_server.sendToClient({niu:'server got client message'}, incomeData.data._sender);
 						
 					}
