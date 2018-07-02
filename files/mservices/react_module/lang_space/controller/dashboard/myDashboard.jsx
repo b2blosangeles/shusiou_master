44c2d99fe711069c6100ef8106ae388ee8d5c2bb
b2blosangeles <b2blosangeles@gmail.com>
2018-07-01 21:59:47 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -43,8 +43,8 @@ try {
 	},
         componentDidUpdate:function(preProps, preState) {
 		let me = this;
-		console.log('me.state.audioChannel--->');
-		console.log(me.state.audioChannel);
+		console.log('me.state.audioClient--->');
+		console.log(me.state.audioClient);
         },	    
         componentDidMount:function() {
 		let me = this;
