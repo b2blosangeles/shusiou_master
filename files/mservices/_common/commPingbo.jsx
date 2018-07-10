16fdac13ae14024dad01daf5b40994de82a4feb7
b2blosangeles <b2blosangeles@gmail.com>
2018-07-10 10:38:26 -0700

    Update commPingbo.jsx

--- files/mservices/_common/commPingbo.jsx
+++ files/mservices/_common/commPingbo.jsx
@@ -2,80 +2,16 @@ try {
 	var _commPingbo = React.createClass({
 		getInitialState: function() {
 			var me = this;
-			if (!_commObj.unicode || _commObj.unicode > 99999) {
-				_commObj.unicode = 1;
+			if (!_commPingbo.unicode || _commPingbo.unicode > 99999) {
+				_commPingbo.unicode = 1;
 			} else {
-				_commObj.unicode++;
+				_commPingbo.unicode++;
 			}	
-		//	console.log(_commObj.unicode);
 			return {};
-		},	
-		videoImageUrl : function() {
-			let me = this, a = me.props.data.rec, ss =  me.props.data.ss, 
-			    size =  (me.props.data.size) ?  me.props.data.size : 480;
-			var url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid +
-				      '&size=' + size + '&ss=' + ss;
-			if (!a.space) return '';
-			return url;
-		},
-		showVideoImage : function(url, classname) {
-			$('.'+classname).hide();
-			$("img[src='" + url + "']").show();
-		},
-		videoImage : function() {
-			var me = this, 
-			    url = me.videoImageUrl(), width = (me.props.data.width) ? me.props.data.width : '',
-			    classname = 'videoImage_' + _commObj.unicode;
-			
-			if (!url) return (<span/>);
-			var def = (width) ? 
-				(<img src={_master_svr() + '/images/empty_default.png'} className={classname} width={width} />) :
-				(<img src={_master_svr() + '/images/empty_default.png'} className={classname} />) 
-			var img = (width) ? 
-				(<img src={url} width={width}  style={{display:'none'}} onLoad={me.showVideoImage.bind(me, url, classname)} />) :
-				(<img src={url} style={{display:'none'}} onLoad={me.showVideoImage.bind(me, url, classname)} />) 
-			
-			return (<span>{img}{def}</span>);
-		},
-		videoBgImage : function() {
-			let me = this, img = me.props.data.img, url =  me.videoImageUrl();
-			return (url) ? (<img src={img} 
-				style={{width:'100%', background:'url('+url+')', 'background-size':'cover'}} />) :  (<span/>);
-		},		
-		video : function() {
-			var me = this;
-			let a = me.props.data.rec, 
-			    ss =  me.props.data.ss, 
-			    t = me.props.data.t,
-			    size =  (me.props.data.size) ?  me.props.data.size : 480,
-			    url;			
-			
-			if (!a.space) return '';
-			if (!ss || !t) url =  _node_svr() + '/api/video/pipe_stream.api?space=' + a.space + '&video_fn='+ a.vid;
-			else url =  _node_svr() + '/api/video/pipe.api?space=' + a.space + '&video_fn='+ a.vid + '&ss=' + ss + '&t=' + t;
-			
-			var video_domid = 'video_' + _commObj.unicode; 
-			setTimeout(
-				function() {
-					let video = $('#' + video_domid);
-					video[0].play();
-					video.click(function(){
-						if (this.paused)  this.play();
-						else this.pause();
-					});
-				}
-			);
-			return (<video id={video_domid} src={url} width={size} loop={true} autoplay={true}>
-			</video>);
-			
 		},		
 		render: function() {
-			let me = this, code = (me.props.data) ? me.props.code : '';
-			if (typeof me[code] === 'function') {
-				return (<span>_commPingbo </span>)
-			} else {
-				return  (<span>{'inorrect code! ' + code}</span>)
-			} 
+			let me = this;
+			return (<span>_commPingbo </span>)
 		}
 	});	
 } catch (err) {
