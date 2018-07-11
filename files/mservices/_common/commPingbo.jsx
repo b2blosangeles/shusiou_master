e03300a96a79da75540120fc27ba47c919626b67
b2blosangeles <b2blosangeles@gmail.com>
2018-07-10 22:25:47 -0700

    Update commPingbo.jsx

--- files/mservices/_common/commPingbo.jsx
+++ files/mservices/_common/commPingbo.jsx
@@ -23,19 +23,13 @@ try {
 				function() {
 					if (new Date().getTime() - me.state.pingbo_tm > 6000 && (me.state.pingbo)) {
 						me.setState({pingbo : null});
-						console.log('GGG');
 					} else if (new Date().getTime() - me.state.pingbo_tm > 3000 && (me.state.pingbo)) {
-						console.log(me.state.pingbo_tm);
-						console.log('HHH');
 						if (me.state.pingbo) {
 							me.props.parent.qna_server.sendToClient({cmd:'pingbo'}, me.state.pingbo);
 						}
-					} else {
-						console.log('===niu===');
-					}
+					} 
 				}, 1000
 			)
-			// me.qna_server.sendToClient({cmd:code, dt:new Date()}, me.qna_server.getClients()[0]);
 		},		
 		componentDidUpdate:function(preProps, preState) {
 			let me = this;
