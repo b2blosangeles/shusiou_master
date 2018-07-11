9af314751b0bce5c47d1ed9c5744c94337b9d13e
b2blosangeles <b2blosangeles@gmail.com>
2018-07-11 06:34:25 -0700

    Update commPingbo.jsx

--- files/mservices/_common/commPingbo.jsx
+++ files/mservices/_common/commPingbo.jsx
@@ -39,6 +39,7 @@ try {
 				//console.log(me.state.socket_id + '==vs===' + preState.socket_id);
 				//console.log(me.state.pingbo + '==ps===' + preState.pingbo);
 			}
+			console.log('==uuppddtteedd==');
 		},
 		componentDidMount:function() {
 			let me = this;
