f2f35bc3c9065f98c7ae249e53850844b6928023
b2blosangeles <b2blosangeles@gmail.com>
2018-08-05 08:41:54 -0700

    Create _commVoiceAI.jsx

---
+++ files/mservices/_common/_commVoiceAI.jsx
@@ -0,0 +1,24 @@
+try {
+	var _commVoiceAI = React.createClass({
+		getInitialState: function() {
+			var me = this;	
+			return {};
+		},
+		componentWillUnmount : function() {
+			let me = this;
+		},	
+		componentDidUpdate:function(preProps, preState) {
+			let me = this;
+		},
+		componentDidMount:function() {
+			let me = this;
+			return true;
+		},
+		render: function() {
+			let me = this;
+			return (<span></span>)
+		}
+	});	
+} catch (err) {
+	console.log(err.message);
+}
