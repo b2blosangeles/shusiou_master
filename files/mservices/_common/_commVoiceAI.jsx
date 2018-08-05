af12d0aeaa1dc0b7fb9c62200b26a610ddade054
b2blosangeles <b2blosangeles@gmail.com>
2018-08-05 08:51:00 -0700

    Update _commVoiceAI.jsx

--- files/mservices/_common/_commVoiceAI.jsx
+++ files/mservices/_common/_commVoiceAI.jsx
@@ -12,7 +12,44 @@ try {
 		},
 		componentDidMount:function() {
 			let me = this;
-			alert('_commVoiceAI')
+			let MOVL = 10,
+			    movl = 0;			
+			let prog = me.props.parent.state.VoiceObj;
+			
+			let  s = Math.ceil(new Date().getTime() * 0.001), t = 0, locked = 0;
+
+			let _itv = setInterval(function(){ 
+				if (Math.ceil(new Date().getTime() * 0.001) - s < 1) {
+					return true;
+				} else {
+					s = Math.ceil(new Date().getTime() * 0.001);
+				}
+				if (!me.state.locked) {
+					t++;
+					if (Object.keys(prog).indexOf(t.toString()) === -1) {
+						if (t > MOVL) {
+							console.log(' === Game Over=== ');
+							clearInterval(_itv);
+							Root.lib.playTTS([{
+								text: 'Good job, nice job, thank you',
+								lang : 'en-US'							
+								}], function() {
+							});						
+						} else {
+							console.log('===componentDidMount===> ' + t);
+						}
+					} else {
+						me.setState({locked : true});
+
+						Root.lib.playTTS(prog[t.toString()], function() {
+							me.setState({locked : false});
+						});
+
+						console.log(' locked --> ' + t.toString());
+					}
+				}
+
+			}, 100);			
 			return true;
 		},
 		render: function() {
