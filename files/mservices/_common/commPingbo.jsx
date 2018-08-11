eb3f3dc2df241496893cd1308834776a103ca849
b2blosangeles <b2blosangeles@gmail.com>
2018-08-11 06:29:23 -0700

    Update commPingbo.jsx

--- files/mservices/_common/commPingbo.jsx
+++ files/mservices/_common/commPingbo.jsx
@@ -23,10 +23,7 @@ try {
 						if (me.state.pingbo) {
 							Root.qna_server.sendToClient({cmd:'pingbo'}, me.state.pingbo);
 						}
-						Root.qna_server.sendToClient({cmd:'voicerecong', uu:1234}, me.state.pingbo);
-						console.log(me.state.pingbo);
-					} 
-					//me.setState({parent_location : me.props.parent.props.location.pathname});
+					}
 				}, 1000
 			)
 		},		
