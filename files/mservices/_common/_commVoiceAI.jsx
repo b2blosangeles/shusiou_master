f1730f04207d4bfc71c136a03dd27344be308991
b2blosangeles <b2blosangeles@gmail.com>
2018-08-11 06:53:14 -0700

    Update _commVoiceAI.jsx

--- files/mservices/_common/_commVoiceAI.jsx
+++ files/mservices/_common/_commVoiceAI.jsx
@@ -41,7 +41,8 @@ try {
 							console.log(' === Game Over=== ');
 							clearInterval(me._itv);
 							// me._stopplay = true;
-							Root.qna_server.sendToClient({cmd:'voicerecong'}, Root.state.pingbo);
+							Root.qna_server.sendToClient({cmd:'voicerecong'
+								voiceRecong:['good', 'nice']}, Root.state.pingbo);
 							me.playTTS([{
 								text: 'Good job, nice job, thank you',
 								lang : 'en-US'							
