7ad622970707da219428797849d38cb1f9938f2b
b2blosangeles <b2blosangeles@gmail.com>
2018-08-10 22:25:18 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -83,8 +83,8 @@ try {
 	sendPP : function() {
 		var me = this;
 			//if (me.props.parent.state.pingbo_service) {
-				Root.qna_server.sendToClient({cmd:'voiceRecong', data:me. state.pingbo_service}, me.state.pingbo);
-				console.log('===serverPush>>>===');
+				Root.qna_server.sendToServer{cmd:'voiceRecong', data:me. state.pingbo_service}, me.state.pingbo);
+				console.log('===serverPush>>>1===');
 				//me.props.parent.setState({serverPush : null});
 			//}	
 	},
