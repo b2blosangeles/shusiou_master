8b7fe11193cacd825a40bae2abbb643c06ad4520
b2blosangeles <b2blosangeles@gmail.com>
2018-08-10 17:03:51 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -45,9 +45,9 @@ try {
 	},
 	channelComm : function() {
 		let me = this;
-		if (!Root.state.socket_id) return true;
+		if (!Root.state.pingbo_id) return true;
 		// let url = "https://comm1.service.dev.shusiou.win/?room=CRON_REPORT_A";
-		let url = 'https://comm1.service.dev.shusiou.win/?socket=' + Root.state.socket_id;
+		let url = 'https://comm1.service.dev.shusiou.win/?socket=' + Root.state.pingbo_id;
 		Root.lib.positionedPopup(url, '','180','180','0','0','yes');
 	}, 
 	componentWillUnmount : function() {
