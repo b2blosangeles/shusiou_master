d8e61520bbe01d96ac28d06cc8df12ce35a2761c
b2blosangeles <b2blosangeles@gmail.com>
2018-08-12 06:26:02 -0700

    Update _commVoiceAI.jsx

--- files/mservices/_common/_commVoiceAI.jsx
+++ files/mservices/_common/_commVoiceAI.jsx
@@ -12,7 +12,7 @@ try {
 		},	
 		componentDidUpdate:function(preProps, preState) {
 			let me = this;
-			if (me.props.voiceObj != preProps.voiceObj) {
+			if (me.props.voiceObj != preProps.voiceObj && (me.props.voiceObj) ) {
 				console.log('===preProps.voiceObj===>>' + preProps.voiceObj);
 				me.playVoiceAI();
 			}
