9be412d20ec4a16d0b3a25fa24d4aaab7ebaff23
b2blosangeles <b2blosangeles@gmail.com>
2018-08-15 12:15:53 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -98,7 +98,7 @@ try {
 							</p>
 							{me.microPhone()}
 							<video width="320" height="240" controls>
-							  <source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4">
+							  <source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4"/>
 							</video>
 							<br/><br/>
 						</div>	
