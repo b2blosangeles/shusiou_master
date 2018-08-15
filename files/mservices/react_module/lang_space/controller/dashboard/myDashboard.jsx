d3de4b07e93139a77df116386d88472a70c73057
b2blosangeles <b2blosangeles@gmail.com>
2018-08-15 12:27:44 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -97,7 +97,7 @@ try {
 									{me.dictionary('Open Security Voice Channel')}</a>
 							</p>
 							{me.microPhone()}
-							<video width="320" height="240" controls>
+							<video id="myVideo" width="320" height="240" controls>
 							  <source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4"/>
 							</video>
 							<br/><br/>
