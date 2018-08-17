a4597b85df05aad82712adec1b7efa7eaddf0cc0
b2blosangeles <b2blosangeles@gmail.com>
2018-08-17 06:06:17 -0700

    Update _commVideoVoiceAI.jsx

--- files/mservices/_common/_commVideoVoiceAI.jsx
+++ files/mservices/_common/_commVideoVoiceAI.jsx
@@ -86,11 +86,10 @@ try {
 		},
 		video: function() {
 			var me = this;
-			return (!Root.state.pingbo && (me.isSpeachRecongnise())) ? (<span></span>)
-			(<span>
-				<video id="myVideo" width="320" height="240" controls>
-					<source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4"/>
-				</video></span>) 
+			return (!Root.state.pingbo && (me.isSpeachRecongnise())) ? (<span></span>) :
+			(<video id="myVideo" width="320" height="240" controls>
+			<source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4"/>
+			</video>) 
 		},
 		playVoiceAIUnit : function(t) {
 			let me = this;
