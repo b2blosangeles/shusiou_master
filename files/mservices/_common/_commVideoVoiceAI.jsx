db933c49037831523003becbe127df0ddc284697
b2blosangeles <b2blosangeles@gmail.com>
2018-08-17 07:13:23 -0700

    Update _commVideoVoiceAI.jsx

--- files/mservices/_common/_commVideoVoiceAI.jsx
+++ files/mservices/_common/_commVideoVoiceAI.jsx
@@ -86,7 +86,7 @@ try {
 		},
 		video: function() {
 			var me = this;
-			return (<video id="myVideo" width="320" height="240" controls>
+			return (<video id="myVideo" width="320" height="240" controls style={display:'none'}>
 			<source src="http://node1.service.dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001" type="video/mp4"/>
 			</video>) 
 		},
