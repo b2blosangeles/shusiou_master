66235780ea8646283eff175ae172e3475cbb6f55
b2blosangeles <b2blosangeles@gmail.com>
2018-08-19 01:14:42 -0700

    Update _commVideoVoiceAI.jsx

--- files/mservices/_common/_commVideoVoiceAI.jsx
+++ files/mservices/_common/_commVideoVoiceAI.jsx
@@ -102,6 +102,7 @@ try {
   			var top = 0;
 			let url = 'https://comm1.service.dev.shusiou.win/?socket=' + Root.state.pingbo_id;
 			Root.lib.positionedPopup(url, '', w, h, top, left,'yes');
+			window.focus();
 		}, 
 		microPhone: function() {
 			var me = this;
