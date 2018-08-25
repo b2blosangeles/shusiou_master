e5a4ccdf615b61ca9ba5620afad51712159ab1aa
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 14:38:08 -0700

    Update myDashboard.jsx

--- files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
+++ files/mservices/react_module/lang_space/controller/dashboard/myDashboard.jsx
@@ -1,5 +1,5 @@
 try {	
-    var MyDemo =  React.createClass({
+    var MyDashboard =  React.createClass({
         getInitialState: function() {
             let me = this;
             return {list:[], text:{}, audioChannel: null, pingbo: '', socket_id:'', commData:'', locked: false};
