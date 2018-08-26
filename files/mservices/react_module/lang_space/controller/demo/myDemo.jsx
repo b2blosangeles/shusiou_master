760f1b125dd3b6e37c5e6152a0a3463d2c732856
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 20:35:59 -0700

    Update myDemo.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemo.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemo.jsx
@@ -158,8 +158,10 @@ try {
 			return (<span>
 				<div className="container">	
 					<div className="col-sm-6 col-lg-6 col-md-6">
+						<a href="#/demo/1808250000000002">1808250000000002</a>
 					</div>
 					<div className="col-sm-6 col-lg-6 col-md-6">
+						<a href="#/demo/1808250000000003">1808250000000003</a>
 					</div>
 				</div>	
 			<span>)
