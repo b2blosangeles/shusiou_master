e5f43ee3ce9d791efe9cde06d5364ffd05ba416d
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:34:32 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -1,12 +1,1 @@
-try {	
-    var MyDemos =  React.createClass({
-        getInitialState: function() {
-            let me = this;
-        },	    
-        render: function() {
-            var me = this;
-	    return  (<span>123</span>)
-		}});	
-} catch (err) {
-	  console.log(err.message);
-}
+
