2328d412203d4bde215ce022b04e3bd1289926d3
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:52:01 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -5,7 +5,7 @@ try {
             return {};
         },
 	newAddThumbnail:function(t) {
-		var url = _master_svr() + '/images/teacher_1.jpg';
+		var url = _master_svr() + '/images/teacher_'+t+'.jpg';
 		return url;
 	},
         render: function() {
@@ -13,9 +13,10 @@ try {
 	    return (<div className="content_section">
 			    <br/><br/>
 				<div className="container"> 
+					<div className="col-sm-1 col-lg-1 col-md-1"></div>
 				    <div className="col-sm-4 col-lg-4 col-md-4">
 					    <div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
-						<img src={me.newAddThumbnail()} style={{width:'100%'}}/>
+						<img src={me.newAddThumbnail(1)} style={{width:'100%'}}/>
 						<div className="video_thumbnail_text pull-right">
 							<a href={'#/demo/1808250000000002'}>
 							<button type="button" 
@@ -26,9 +27,10 @@ try {
 						</div>
 					</div>
 				    </div>
+					<div className="col-sm-2 col-lg-2 col-md-2"></div>
 				    <div className="col-sm-4 col-lg-4 col-md-4">
 					    <div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
-						<img src={me.newAddThumbnail()} style={{width:'100%'}}/>
+						<img src={me.newAddThumbnail(2)} style={{width:'100%'}}/>
 						<div className="video_thumbnail_text pull-right">
 							<a href={'#/demo/1808250000000003'}>
 							<button type="button" 
@@ -39,6 +41,7 @@ try {
 						</div>
 					</div>
 				    </div>
+					<div className="col-sm-1 col-lg-1 col-md-1"></div>
 				</div>  
 				<div className="content_bg"></div>
 				{Root.lib.landingModal(me)}
