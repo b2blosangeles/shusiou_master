ccd572ee4a0f5ff043ab89fc7db39eb528b8fc5c
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:40:42 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -25,7 +25,19 @@ try {
         },
         render: function() {
             var me = this;
-	    return (<span></span>)
+	    return (<div className="content_section">
+				<div className="container"> 
+				    <div className="col-sm-6 col-lg-6 col-md-6">
+					<a href="#/demo/1808250000000002">1808250000000002</a>
+				    </div>
+				    <div className="col-sm-6 col-lg-6 col-md-6">
+					<a href="#/demo/1808250000000003">1808250000000003</a>
+				    </div>
+				</div>  
+				<div className="content_bg"></div>
+				{Root.lib.landingModal(me)}
+				
+			</div>)
 	}});	
 } catch (err) {
 	  console.log(err.message);
