64370e1e1bc3c0c8ed731e162bc174ae6357112f
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:33:14 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -5,16 +5,7 @@ try {
         },	    
         render: function() {
             var me = this;
-	    return  (<span>
-				<div className="container">	
-					<div className="col-sm-6 col-lg-6 col-md-6">
-						<a href="#/demo/1808250000000002">1808250000000002</a>
-					</div>
-					<div className="col-sm-6 col-lg-6 col-md-6">
-						<a href="#/demo/1808250000000003">1808250000000003</a>
-					</div>
-				</div>	
-			</span>)
+	    return  (<span>123</span>)
 		}});	
 } catch (err) {
 	  console.log(err.message);
