b10a86378987cc268377d5154107c6cc52f6e673
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:38:40 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -2,7 +2,7 @@ try {
     var MyDemos =  React.createClass({
         getInitialState: function() {
             let me = this;
-            return {list:[], text:{}, audioChannel: null, pingbo: '', socket_id:'', commData:'', locked: false};
+            return {};
         },
 	dictionary:function(v) {
 		if (!this.props.route || !this.props.route.env ||!this.props.route.env.dictionary) return v;
@@ -10,17 +10,6 @@ try {
 	},
 	getCurrentLanguage: function() {
 		return this.props.route.env.getCurrentLanguage();	
-	},
-	getText:function(v) {
-		return this.state.text[this.getCurrentLanguage() + '/home_page/'+v];
-	},
-	textStyle:function() {
-		var me = this;
-		if (me.props.route.env.state.Breakpoint == 'sm' || me.props.route.env.state.Breakpoint == 'sx') {
-			return {'font-size':'0.8em'}
-		} else {
-			return {'font-size':'1em'}	
-		}
 	},
         componentDidUpdate:function(prePropos, preState) {
 		let me = this;
@@ -32,105 +21,11 @@ try {
 	},		    
 	    
         componentDidMount:function() {
-		let me = this;
-		me.setState({
-			videoUrl: '//node1_dev.shusiou.win/api/video/pipe_stream.api?space=https://shusiouwin-dev-1.s3.wasabisys.com/&video_fn=1808090000000001',
-			//videoSetting : {width:480, height:320},
-			script : {'21' : [
-				{
-					length: 3,
-					tts: '深圳打响楼市个人限卖第一枪',
-					lang : 'cmn-Hans-CN'
-				},			
-				{
-					length: 2,
-					//tts: 'please repeat',
-					sr: ['good'],
-					v : {
-						Y : {
-							tts: '很好',
-							lang : 'cmn-Hans-CN'
-						},
-						N: {
-							tts: '不对, 请重试',
-							lang : 'cmn-Hans-CN'
-						}
-					},
-					lang : 'en-US'
-				}
-			],
-			'12' : [
-				{
-				length: 2,
-				tts: '中央定调下半年中国经济',
-				lang : 'cmn-Hans-CN'			
-				},{
-					length: 2,
-					//tts: 'please repeat',
-					sr: ['nice'],
-					v : {
-						Y : {
-							tts: '很好',
-							lang : 'cmn-Hans-CN'
-						},
-						N: {
-							tts: '不对, 请重试',
-							lang : 'cmn-Hans-CN'
-						}
-					},
-					lang : 'en-US'
-				}			
-			],  
-			'18' : [
-				{
-					length: 2,
-					tts: '稳中有变',
-					lang : 'cmn-Hans-CN'			
-				},			
-				{
-					length: 2,
-					//tts: 'please repeat',
-					sr: ['one'],
-					v : {
-						Y : {
-							tts: '很好',
-							lang : 'cmn-Hans-CN'
-						},
-						N: {
-							tts: '不对, 请重试',
-							lang : 'cmn-Hans-CN'
-						}
-					},
-					lang : 'en-US'
-				}]},
-			afterScript : {
-				tts: '内容完成',
-				lang : 'cmn-Hans-CN',
-				continuePlay : true
-			},    
-			_tm : new Date().getTime()	    
-		});
-		return true;
+
         },
-	/*
-	releaseHold : function(data) {
-		var me = this;
-		me.setState({locked : false});
-		return (data === 'string') ? data : JSON.stringify(data);
-	}, */
         render: function() {
             var me = this;
-	    return (<div className="content_section">
-				<br/>
-				<div className="container">
-					<div className="col-sm-12 col-lg-12 col-md-12"> 
-						<_commVideoVoiceAI parent={me} TM={me.state._tm}/>
-					</div>					
-				</div> 
-				<div className="content_bg"></div>
-				{Root.lib.landingModal(me)}
-				
-			</div>)
+	    return (<span></span>)
 	}});	
 } catch (err) {
 	  console.log(err.message);
