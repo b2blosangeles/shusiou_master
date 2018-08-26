67386dcf7886026a6c4ff0a3b8c3d294ffed9547
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:42:08 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -4,25 +4,6 @@ try {
             let me = this;
             return {};
         },
-	dictionary:function(v) {
-		if (!this.props.route || !this.props.route.env ||!this.props.route.env.dictionary) return v;
-		return this.props.route.env.dictionary(v);
-	},
-	getCurrentLanguage: function() {
-		return this.props.route.env.getCurrentLanguage();	
-	},
-        componentDidUpdate:function(prePropos, preState) {
-		let me = this;
-		return true;
-        },
-	componentWillUnmount : function() {
-		let me = this;
-
-	},		    
-	    
-        componentDidMount:function() {
-
-        },
         render: function() {
             var me = this;
 	    return (<div className="content_section">
