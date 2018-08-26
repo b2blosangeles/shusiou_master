843942d6d002f41f204334190f048251227d2bfa
b2blosangeles <b2blosangeles@gmail.com>
2018-08-25 21:45:46 -0700

    Update myDemos.jsx

--- files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
+++ files/mservices/react_module/lang_space/controller/demo/myDemos.jsx
@@ -9,10 +9,30 @@ try {
 	    return (<div className="content_section">
 				<div className="container"> 
 				    <div className="col-sm-6 col-lg-6 col-md-6">
-					<a href="#/demo/1808250000000002">1808250000000002</a>
+					    <div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
+						<img src={me.newAddThumbnail()} style={{width:'100%'}}/>
+						<div className="video_thumbnail_text pull-right">
+							<a href={'#/demo/1808250000000002'}>
+							<button type="button" 
+								className="btn btn-warning">
+								{Root.lib.dictionary('demo 1')}
+							</button>
+							</a>	
+						</div>
+					</div>
 				    </div>
 				    <div className="col-sm-6 col-lg-6 col-md-6">
-					<a href="#/demo/1808250000000003">1808250000000003</a>
+					    <div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
+						<img src={me.newAddThumbnail()} style={{width:'100%'}}/>
+						<div className="video_thumbnail_text pull-right">
+							<a href={'#/demo/1808250000000003'}>
+							<button type="button" 
+								className="btn btn-warning">
+								{Root.lib.dictionary('demo 2')}
+							</button>
+							</a>	
+						</div>
+					</div>
 				    </div>
 				</div>  
 				<div className="content_bg"></div>
