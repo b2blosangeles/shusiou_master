926bcf75466750dba601d6db85cf45f9a37d1eaf
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 07:08:11 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -12,13 +12,16 @@ try {
 		},
 		showMenu : function()  {
 			var me = this;
-			return ({me.state.options.map(function(m){ 
-				return(<a className="btn btn-success" 
-						href={'#/tutor/addVideo'} style={{'margin':'0.5em'}}>
-						<i className="fa fa-upload" aria-hidden="true"></i>
-						&nbsp;{Root.lib.dictionary(m.code)}===---
-					</a>)	
-			})});		
+			return (<span>
+					{me.state.options.map(function(m){ 
+						return(<a className="btn btn-success" 
+								href={'#/tutor/addVideo'} style={{'margin':'0.5em'}}>
+								<i className="fa fa-upload" aria-hidden="true"></i>
+								&nbsp;{Root.lib.dictionary(m.code)}===---
+							</a>)	
+						})
+					}
+				</span>);		
 		},
 		closeAdmin:function(v) {
 			var me = this;
