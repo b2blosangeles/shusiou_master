bf352d94558855f23fb4ecdc35bbb1f9d8760789
b2blosangeles <b2blosangeles@gmail.com>
2018-11-10 23:25:55 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -18,7 +18,7 @@ try {
 						<div className="col-sm-12 col-lg-12 col-md-12">
 							<div className="overlayer_box homepage_box" 
 								style={{'margin-bottom':'1em', 
-									'padding':'0.5em', 'min-height':'60em'}}>
+									'padding':'0.5em', 'min-height':'36em'}}>
 								<a className="btn btn-success" 
 									href={'#/tutor/addVideo'}>
 									{Root.lib.dictionary('upload_video')}
