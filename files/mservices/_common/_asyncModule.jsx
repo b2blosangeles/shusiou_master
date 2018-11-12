139270a3e42bfb400410ab5166a648b0d952eb4f
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 08:22:45 -0800

    Update _asyncModule.jsx

--- files/mservices/_common/_asyncModule.jsx
+++ files/mservices/_common/_asyncModule.jsx
@@ -35,9 +35,9 @@ try {
 			if (me.state.success === false) {
 				return  (<span>Loading Failure!</span>)
 			} else if (me._asyncModule) {
-				var asyncObj = me._asyncModule;
+				var ab = me._asyncModule;
 				var v = new me._asyncModule({}); // {v.render()}  value={'5678'} aria-describedby="basic-addon2"
-				return  (<span><asyncObj/></span>)
+				return  (<span><ab/></span>)
 			} else {
 				return  (<span>Loading ...</span>)
 			}
