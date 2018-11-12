a57419519334400d41ceac27ae05ae79946fa573
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 09:34:56 -0800

    Update myVideo.js

--- api/video/myVideo.js
+++ api/video/myVideo.js
@@ -168,7 +168,7 @@ var app = function(auth_data) {
 		case 'getYouTubeInfo':
 			ytdl.getInfo(req.body.video_url, {},  function(err, info){
 			  if ((err) || !info) {
-			    res.send(err);
+			    res.send({err:err, info:info});
 			  } else {
 			    var r = {vid:info.video_id, title:info.title, length_seconds:parseInt(info.length_seconds), thumbnail_url:info.thumbnail_url};
 			    res.send({status:'success', data:r});
