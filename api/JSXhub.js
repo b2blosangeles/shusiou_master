472b660af643badc41869e2f8462461eb62cdd09
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 21:02:55 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -1,3 +1,7 @@
 var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
 var qaletBabel = new Babel();
-res.send(req.query.url + '====');
+var fn = req.query.url;
+qaletBabel.jsx2js(fn, function(err, v) {
+    res.send(req.query.url + '====');
+});
+
