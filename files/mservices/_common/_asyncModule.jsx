af65421332e7518e91d54dc18281e1e4def906c8
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 13:28:35 -0800

    Update _asyncModule.jsx

--- files/mservices/_common/_asyncModule.jsx
+++ files/mservices/_common/_asyncModule.jsx
@@ -1,40 +1,4 @@
 try {
-
-	var _asyncModule = React.createClass({
-		getInitialState: function() {
-			var me = this;
-			return {};
-		},
-		componentDidMount:function(prevState, prevProps) {
-			var me = this;
-			me.loadCode();
-		},		
-		componentDidUpdate:function(prevProps, prevState) {
-			var me = this;
-			if (me.props.url !== prevProps.url) { 
-				me.loadCode();
-			}	
-		},
-		loadCode : function() {
-			var me = this;
-			var url = _master_svr() + '/api/JSXhub.api?url=' + encodeURIComponent(me.props.url) + 
-			    	'&tm=' + new Date().getTime();
-			$.get(url, function(data, status){
-				if (data.success)  {
-					me._asyncModule = data.code;
-					me.setState({success: true, update : new Date().getTime()});
-				} else {
-					me._asyncModuleErr = data.err;
-					me.setState({success: false, update : new Date().getTime()});
-				}
-				
-			});			
-		},
-		render: function() {
-			var me = this;
-			if (me.state.success === false) {
-				return  (<span>Script Error: {me._asyncModuleErr}</span>)
-			} else if (me._asyncModule) {
 //====
 					var _asyncOBJ = React.createClass({
         getInitialState: function() {
@@ -124,6 +88,42 @@ try {
         }
 });
 //====
+	var _asyncModule = React.createClass({
+		getInitialState: function() {
+			var me = this;
+			return {};
+		},
+		componentDidMount:function(prevState, prevProps) {
+			var me = this;
+			me.loadCode();
+		},		
+		componentDidUpdate:function(prevProps, prevState) {
+			var me = this;
+			if (me.props.url !== prevProps.url) { 
+				me.loadCode();
+			}	
+		},
+		loadCode : function() {
+			var me = this;
+			var url = _master_svr() + '/api/JSXhub.api?url=' + encodeURIComponent(me.props.url) + 
+			    	'&tm=' + new Date().getTime();
+			$.get(url, function(data, status){
+				if (data.success)  {
+					me._asyncModule = data.code;
+					me.setState({success: true, update : new Date().getTime()});
+				} else {
+					me._asyncModuleErr = data.err;
+					me.setState({success: false, update : new Date().getTime()});
+				}
+				
+			});			
+		},
+		render: function() {
+			var me = this;
+			if (me.state.success === false) {
+				return  (<span>Script Error: {me._asyncModuleErr}</span>)
+			} else if (me._asyncModule) {
+
 				// return  (<span><NNBB parent={me.props.parent}/></span>)
 				try {
 					// var _asyncOBJ = new Function(decodeURIComponent(me._asyncModule));
