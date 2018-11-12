047dfd1d31e91415dbc82b224b3740ec0ee6aed3
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 10:42:24 -0800

    Update main.jsx

--- files/js/module/pullYouTube/main.jsx
+++ files/js/module/pullYouTube/main.jsx
@@ -1,7 +1,7 @@
 React.createClass({
         getInitialState: function() {
                 var me = this;
-                return {video_url:'', vid:'', error:'', list:[], method: new Date().getTime()};
+                return {video_url:'',  error:'', list:[], method: new Date().getTime()};
         },
         initState:function() {
            //     this.setState({video_url:'', vid:'', title:'', length_seconds:0,thumbnail_url:'', error:''});			
