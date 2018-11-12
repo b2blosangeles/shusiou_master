3320952402bd924bb939031eb1e915049c8e4177
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 09:08:48 -0800

    Update main.jsx

--- files/js/module/pullYouTube/main.jsx
+++ files/js/module/pullYouTube/main.jsx
@@ -47,7 +47,7 @@ React.createClass({
         pullingYoutube : function () {
                 var me = this;
                 return (<p  style={{'padding':'1em'}}>				
-                        <h5>YouTube Video Url</h5>	
+                        <h5>YouTube Video URL</h5>	
                         <div className="input-group">
                           <input type="text" className="form-control" placeholder="Input YouTube link" 
                                   value={me.state.video_url}
