b25a2ea3ad6f336e2410f0102eb103b8945cf22e
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 21:29:04 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -2,7 +2,10 @@ var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
 var qaletBabel = new Babel();
 var fn = env. site_path + decodeURIComponent(req.query.url);
 qaletBabel.jsx2js(fn, function(err, v) {
-       res.send({code: encodeURIComponent('me._pluginObj = ' + v.code)});
-    // res.send(req.query.url + '====');
+       if (err) {
+              res.send({success: false, err:err.message. })
+       } else {
+              res.send({success: true, code: encodeURIComponent('me._pluginObj = ' + v.code)});
+       }
 });
 
