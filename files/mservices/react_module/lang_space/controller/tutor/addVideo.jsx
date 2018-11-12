15915cbeb4a03acf5a6a6cfd0424b48fe3a52319
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 23:01:08 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -39,11 +39,9 @@ try {
 			var me = this;
 			for (var i = 0; i < me.options.length; i++) {
 				if (me.options[i].code === me.state.option) {
-					return (
-						<div className="overlayer_box homepage_box" style={{'margin-top': '0.5em'}}>
-							<_asyncModule url={me.options[i].url} />
-						</div>);
-					)
+					return (<div className="overlayer_box homepage_box" style={{'margin-top': '0.5em'}}>
+						<_asyncModule url={me.options[i].url} />
+					</div>)
 				}
 			}
 			return (<span />)			
