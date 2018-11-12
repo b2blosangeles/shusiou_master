b50596850732e212b75d70f2a21d25e1b3a9c8c8
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 19:07:40 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -50,7 +50,7 @@ try {
 						return  (<span>{me.state.niu}</span>)
 					}
 				});
-				return (<_pluginObj obj={(<_v/>)} />)
+				return (<_pluginObj url={'/js/testy.js'} />)
 			},
 			'pull_youtube': function() {
 				return 'Pull YouTube'
