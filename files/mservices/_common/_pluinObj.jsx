964ab22456e7e3b0bc17c1f5ee186b5e95ece34e
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 16:33:28 -0800

    Create _pluinObj.jsx

---
+++ files/mservices/_common/_pluinObj.jsx
@@ -0,0 +1,21 @@
+try {
+	var _pluinObj = React.createClass({
+		getInitialState: function() {
+			var me = this;
+      /*
+			if (!_commObj.unicode || _commObj.unicode > 99999) {
+				_commObj.unicode = 1;
+			} else {
+				_commObj.unicode++;
+			}	
+		//	console.log(_commObj.unicode);
+    */
+			return {};
+		},	
+		render: function() {
+				return  (<span>_pluinObj</span>)
+		}
+	});	
+} catch (err) {
+	console.log(err.message);
+}
