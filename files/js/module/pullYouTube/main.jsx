3d175d5b8cb20dc711eb47821d8ce71eb09e8475
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 06:48:21 -0800

    Update main.jsx

--- files/js/module/pullYouTube/main.jsx
+++ files/js/module/pullYouTube/main.jsx
@@ -59,7 +59,6 @@ React.createClass({
         },        
         render: function() {
           var me = this;
-          return  (<span><input type="text" className="form-control" placeholder="Input YouTube link" 
-                           /></span>)
+          return  (<span>{me.pullingYoutube()}</span>)
         }
 });
