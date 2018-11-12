cc3a9827808afd95fd7511bed3c5d2e9428ce982
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 17:31:31 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -60,7 +60,7 @@ try {
 						}
 					});
 				// return (<_v/>)
-				return (<_pluginObj obj=(<v/>) />)
+				return (<_pluginObj obj={(<v/>)} />)
 			},
 			'pull_youtube': function() {
 				return 'Pull YouTube'
