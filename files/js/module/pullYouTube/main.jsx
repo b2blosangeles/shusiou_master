c79b17e26cd76c934176671da90f30402f932712
b2blosangeles <b2blosangeles@gmail.com>
2018-11-12 07:10:37 -0800

    Update main.jsx

--- files/js/module/pullYouTube/main.jsx
+++ files/js/module/pullYouTube/main.jsx
@@ -58,7 +58,7 @@ React.createClass({
                                   aria-describedby="basic-addon2"/>
                           <div className={me.videoUrlSubmitable()} onClick={me.videoUrlDecode.bind(me)}>Submit</div>
                         </div>
-                        <hr/>{me.state.video_url} <hr/>
+                        <hr/>=={me.state.video_url}== <hr/>
                                 
                         <p>
                                 <h5>Or pulling a shared videos</h5>
