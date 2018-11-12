b0276a111ce2cfa89a799692e82b2fa890f48d6c
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 23:31:39 -0800

    Update main.jsx

--- files/js/module/pullYouTube/main.jsx
+++ files/js/module/pullYouTube/main.jsx
@@ -3,13 +3,48 @@ React.createClass({
           var me = this;
           return {}
         },
-        pp: function() {
-          alert('Pull YouTube');
-        },
+        handleChange:function(e) {
+                var me = this;
+                me.setState({video_url:e.target.value});
+        },        
+        videoUrlDecode:function() {
+                var me = this, code = me.state.video_url;		
+                let engCfg = {
+                        request:{code:'getVieoInfo', 
+                                 url :  _master_svr() + '/api/video/myVideo.api?opt=getYouTubeInfo', 
+                                 method:'post', 
+                                 data:{video_url: me.state.video_url}
+                        },
+                        hold:500,
+                        setting: {timeout:6000},
+                        callBack: function(data) {
+                                data.data.code = code;
+                                me.setState(data.data);
+                        }
+                }			
+                Root.lib.loadEng(me, engCfg);
+        },        
+        pullingYoutube : function () {
+                var me = this;
+                return (<p  style={{'padding':'1em'}}>				
+                        <h5>Pulling YouTube Video --- Upload video</h5>	
+                        <div className="input-group">
+                          <input type="text" className="form-control" placeholder="Input YouTube link" 
+                                  value={me.state.video_url}
+                                  onChange={this.handleChange.bind(this)}
+                                  aria-describedby="basic-addon2"/>
+                          <div className={me.videoUrlSubmitable()} onClick={me.videoUrlDecode.bind(me)}>Submit</div>
+                        </div>
+                        <hr/>
+                        <p>
+                                <h5>Or pulling a shared videos</h5>
+                        </p>
+                </p>)
+        },        
         render: function() {
           var me = this;
           return  (<span>
-                          Pull YouTube <a onClick={me.pp.bind(me)} >test</a>
+                          Pull YouTube 
                  </span>)
         }
 });
