b890096fc76df0fb1d7f5d7e0e6ba2ed711e8643
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 22:25:12 -0800

    Update main.jsx

--- files/js/module/addSharedVideo/main.jsx
+++ files/js/module/addSharedVideo/main.jsx
@@ -1,1 +1,17 @@
 
+
+React.createClass({
+        getInitialState: function() {
+          var me = this;
+          return {}
+        },
+        pp: function() {
+          alert('Add Shared Video');
+        },
+        render: function() {
+          var me = this;
+          return  (<span>
+                          Add Shared Video <a onClick={me.pp.bind(me)} >test</a>
+                 </span>)
+        }
+});
