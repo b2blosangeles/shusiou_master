43b2e3e69c41d498846b4b1805abb28560a9da58
b2blosangeles <b2blosangeles@gmail.com>
2018-11-11 22:29:08 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -39,13 +39,13 @@ try {
 		OptionBody : {
 			'upload_video': function() {
 				var me = this;
-				return (<_asyncModule url={'/files/js/module/fileUpload/main.jsx'} />)
+				return (<_asyncModule url={'/files/js/module/fileUpload/main.jsx'} parent={me} />)
 			},
 			'pull_youtube': function() {
-				return (<_asyncModule url={'/files/js/module/pullYouTube/main.jsx'} />)
+				return (<_asyncModule url={'/files/js/module/pullYouTube/main.jsx'} parent={me} />)
 			},
 			'add_shared_video': function() {
-				return (<_asyncModule url={'/files/js/module/addSharedVideo/main.jsx'} />)
+				return (<_asyncModule url={'/files/js/module/addSharedVideo/main.jsx'} parent={me} />)
 			}			
 		},
 		showOptionBody : function() {
