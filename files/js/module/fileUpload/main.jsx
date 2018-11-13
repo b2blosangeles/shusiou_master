4cead862a76164617d02bbca91510f2dd86e19e6
b2blosangeles <b2blosangeles@gmail.com>
2018-11-13 15:55:30 -0800

    Update main.jsx

--- files/js/module/fileUpload/main.jsx
+++ files/js/module/fileUpload/main.jsx
@@ -308,7 +308,7 @@ React.createClass({
                                   <br/><hr/>
                                       <a id="dbi-file-upload-submit" href="Javascript:void(0)">test</a>
                                       <hr/>
-                          <div className="progress" style={{'height': '20px'}}>
+                          <div className="progress" style={{'height': '2px'}}>
                                 <div className="progress-bar" role="progressbar" style={{width: me.state.percentDone}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                          </div>
                                         <hr/><br/>
