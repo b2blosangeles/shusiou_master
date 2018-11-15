bfdcc2fe459c769c6e66bbd509f211c697b1a598
b2blosangeles <b2blosangeles@gmail.com>
2018-11-14 23:46:49 -0800

    Update test.jsx

--- files/js/module/addSharedVideo/test.jsx
+++ files/js/module/addSharedVideo/test.jsx
@@ -1,1 +1,12 @@
-alert('test.jsx');
+ var testList = React.createClass({
+        getInitialState: function() {
+          var me = this;
+          return {}
+        },
+        render: function() {
+          var me = this;
+          return  (<span>
+                         testList
+                 </span>)
+        }
+});
