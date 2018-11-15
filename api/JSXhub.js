ea63d0bf8670a242a2cbba5fc5a8e4a15c7def49
b2blosangeles <b2blosangeles@gmail.com>
2018-11-14 17:23:20 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -24,7 +24,7 @@ if ((req.body.includes) && (req.body.includes.length)) {
                                                  if (err) {
                                                         cbk({success: false, err:err.message})
                                                  } else {
-                                                        cbk({success: true, code: encodeURIComponent(v.code)});
+                                                        cbk({success: true, code: v.code});
                                                  }
                                           });
                                    }
@@ -33,5 +33,19 @@ if ((req.body.includes) && (req.body.includes.length)) {
        
 }
 cp.serial(_f, function(data) {
-       res.send(data);             
+       var inc_str = '', master_str = '', error = [];
+       for (var i = 0; i < req.body.includes.length; i++) {
+              if (cp.data['includes+' + i].success === true) {
+                     inc_str += p.data['includes+' + i].code;
+              } else {
+                     error.push(p.data['includes+' + i].err);
+              }
+       }
+       if (cp.data.master.success === true) {
+           master_str += p.data.master.code
+              
+       } else {
+               error.push(p.data.master.err)
+       }
+       res.send({master: master_str, includes: inc_str, err : error});             
 }, 3000);
