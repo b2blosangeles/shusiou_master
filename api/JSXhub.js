03042f35eed2127ae518e7a340c3dc55dfd017f2
b2blosangeles <b2blosangeles@gmail.com>
2018-11-14 16:56:52 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -1,13 +1,20 @@
 var Babel  = require(env.root_path + "/package/qaletBabel/qaletBabel.js");
 var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 
-var qaletBabel = new Babel();
-var fn = env. site_path + decodeURIComponent(req.body.main);
-qaletBabel.jsx2js(fn, function(err, v) {
-       if (err) {
-              res.send({success: false, err:err.message})
-       } else {
-              res.send({success: true, code: encodeURIComponent(v.code)});
-       }
-});
+var cp = new CP();
+var _f = [];
 
+_f.master = function(cbk) {
+       var qaletBabel = new Babel();
+       var fn = env. site_path + decodeURIComponent(req.body.main);
+       qaletBabel.jsx2js(fn, function(err, v) {
+              if (err) {
+                     cbk({success: false, err:err.message})
+              } else {
+                     cbk({success: true, code: encodeURIComponent(v.code)});
+              }
+       });
+}
+cp.serial(_f, function(data) {
+       res.send(cp.data.master);             
+}, 3000);
