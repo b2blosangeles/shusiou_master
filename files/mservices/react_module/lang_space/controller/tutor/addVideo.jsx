ee6eda3f38f7b70673407064d0c413dbd728e510
b2blosangeles <b2blosangeles@gmail.com>
2018-11-14 18:03:49 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -8,7 +8,7 @@ try {
 						main : '/files/js/module/fileUpload/main.jsx'
 					},
 					'pull_youtube' : {
-						includes : ['/files/js/module/pullYouTube/test.jsx'],
+						includes : ['/files/js/module/pullYouTube/master.jsx'],
 						main : '/files/js/module/pullYouTube/main.jsx'
 					},
 					'add_shared_video' : {
