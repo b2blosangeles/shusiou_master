620c8f9c64d90dadea41702ac6dfabd51ebad455
b2blosangeles <b2blosangeles@gmail.com>
2018-11-16 15:08:06 -0800

    Create dataList.jsx

---
+++ files/js/module/publicNews/dataList.jsx
@@ -0,0 +1,22 @@
+var dataList = React.createClass({
+        getInitialState: function() {
+          var me = this;
+          return {}
+        },
+        goBackMyVideos: function() {
+                window.location.href = '#/tutor/my_videos';
+        },
+        componentDidMount:function() {
+               var me = this;
+               me.init();
+        },        
+        init : function() {
+                var meObj = this;
+        },
+        render: function() {
+          var me = this;
+          return  (<span>
+                        dataList                      
+                 </span>)
+        }
+});
