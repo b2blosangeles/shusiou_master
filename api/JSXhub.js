72aa1edd51a6a8c05aab4273d33de36ddb2c77eb
b2blosangeles <b2blosangeles@gmail.com>
2018-11-17 06:37:10 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -3,10 +3,12 @@ var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 
 var cp = new CP();
 var _f = [];
+var _includes = (req.body.includes) ? req.body.includes : [], 
+    _main = (req.body.main) ? req.body.main : '';
 
 _f.master = function(cbk) {
        var qaletBabel = new Babel();
-       var fn = env. site_path + decodeURIComponent(req.body.main);
+       var fn = env. site_path + decodeURIComponent(_main);
        qaletBabel.jsx2js(fn, function(err, v) {
               if (err) {
                      cbk({success: false, err:err.message})
@@ -16,34 +18,34 @@ _f.master = function(cbk) {
        });
 }
 
-if ((req.body.includes) && (req.body.includes.length)) {
-      for (var i = 0; i < req.body.includes.length; i++) {
-          _f['inc_' + i] = (function(i) { return function(cbk) {
-                                   var qaletBabel = new Babel();
-                                   var fn = env. site_path + decodeURIComponent(req.body.includes[i]);
-                                          qaletBabel.jsx2js(fn, function(err, v) {
-                                                 if (err) {
-                                                        cbk({success: false, err:err.message});
-                                                 } else {
-                                                        cbk({success: true, code: encodeURIComponent(v.code)});
-                                                 }
-                                          });
-                                   }
-                            })(i)
-      }
-       
+
+for (var i = 0; i < _includes.length; i++) {
+   _f['inc_' + i] = (function(i) { return function(cbk) {
+              var qaletBabel = new Babel();
+              var fn = env. site_path + decodeURIComponent(_includes[i]);
+                     qaletBabel.jsx2js(fn, function(err, v) {
+                            if (err) {
+                                   cbk({success: false, err:err.message});
+                            } else {
+                                   cbk({success: true, code: encodeURIComponent(v.code)});
+                            }
+                     });
+              }
+       })(i)
 }
+       
+
 cp.serial(_f, function(data) {
        var inc_str = '', master_str = '', err = [];
-       if ((req.body.includes) && (req.body.includes.length)) {
-              for (var i = 0; i < req.body.includes.length; i++) {
-                     if (cp.data['inc_' + i].success === true) {
-                            inc_str += cp.data['inc_' + i].code;
-                     } else {
-                            err.push(cp.data['inc_' + i].err);
-                     }
+  
+       for (var i = 0; i < _includes.length; i++) {
+              if (cp.data['inc_' + i].success === true) {
+                     inc_str += cp.data['inc_' + i].code;
+              } else {
+                     err.push(cp.data['inc_' + i].err);
               }
        }
+
        if (cp.data.master.success === true) {
            master_str = cp.data.master.code 
        } else {
