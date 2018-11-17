375033bc563beb7b9e23eb7b6ef637381f0a6e5b
b2blosangeles <b2blosangeles@gmail.com>
2018-11-17 06:24:44 -0800

    Create dataList1.js

---
+++ files/js/module/pub1/dataList1.js
@@ -0,0 +1,15 @@
+var _dataList = React.createClass({
+        getInitialState: function() {
+          var me = this;
+          return {}
+        },
+        componentDidMount:function() {
+               var me = this;
+        },        
+        render: function() {
+          var me = this;
+          return  (<span>
+                        dataList 1                     
+                 </span>)
+        }
+});
