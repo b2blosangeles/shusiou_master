3681945cb29fca6c6bb9d4f8d35b1c5bc5b8e24c
b2blosangeles <b2blosangeles@gmail.com>
2018-11-17 05:57:04 -0800

    Update addVideo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/addVideo.jsx
@@ -54,8 +54,8 @@ try {
 				if (code === me.state.option) {
 					return (<div className="overlayer_box homepage_box" 
 							style={{'margin-top': '0.5em', 'min-height':'18em'}}>
-						<_asyncModule plugin={extend: me.options[code], 
-									master: '//master1_dev.shusiou.win/api/JSXhub.api'} code={code} parent={me} />
+						<_asyncModule plugin={{extend: me.options[code], 
+								master: '//master1_dev.shusiou.win/api/JSXhub.api'}} code={code} parent={me} />
 					</div>)
 				}
 			}
