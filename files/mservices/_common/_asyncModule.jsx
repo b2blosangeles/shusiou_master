e2bb778a9523fee4dd0e0555e1edb3d2c32555c6
b2blosangeles <b2blosangeles@gmail.com>
2018-11-17 01:00:50 -0800

    Update _asyncModule.jsx

--- files/mservices/_common/_asyncModule.jsx
+++ files/mservices/_common/_asyncModule.jsx
@@ -78,7 +78,7 @@ try {
 				try {
 					var _asyncOBJ = React.createClass({render: function() { return (<span/>)}});
 					eval( decodeURIComponent(me._asyncModule.inc));
-					console.log('_asyncOBJ = ' + decodeURIComponent(me._asyncModule.code));
+					console.log('_asyncOBJ = ' + decodeURIComponent(me._asyncModule.master));
 					return  (<span>
 							<_asyncOBJ parent={me}/>
 							{/*Root.lib.landingModal(me)*/}
