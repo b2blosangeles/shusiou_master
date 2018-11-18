190d82a2926a43b455ebf0b12e1a99d8be7ebfce
b2blosangeles <b2blosangeles@gmail.com>
2018-11-17 20:01:37 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -5,13 +5,14 @@ function cache_request(url, fn, cbk) {
 	pkg.fs.stat(fn, function(err, stats) {
 		if (err || (new Date().getTime() - stats.mtime) > ((req.body.timeout) ? req.body.timeout : 60000)) {
 			pkg.request(url, {rejectUnauthorized: false}, function (err, response, body) {
+				
 				if (!err) {
 					pkg.fs.writeFile(fn, body, function (err) {
 					  if (err) cbk(false);
 					  else  cbk(true);
 					})
 				} else {
-					cbk(false);
+					cbk(response);
 				}
 			})
 		} else {
