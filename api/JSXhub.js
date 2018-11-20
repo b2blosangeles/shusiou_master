269297dd7aa17f5338143e808507286dd9d42fbf
b2blosangeles <b2blosangeles@gmail.com>
2018-11-20 10:44:23 -0800

    Update JSXhub.js

--- api/JSXhub.js
+++ api/JSXhub.js
@@ -140,7 +140,7 @@ cp.serial(_f, function(data) {
        } else {
                err.push(cp.data.master.err);
        }
-	 res.send({success:true, masterUrl:req.body.main, inc: inc_str, master : master_str, err : (_error.length) ? _error : err});
+	 res.send({success:true, masterUrl:_main, inc: inc_str, master : master_str, err : (_error.length) ? _error : err});
       // res.send({p:cp.data.pre, success:true, inc: inc_str, master : master_str, err : (_error.length) ? _error : err}); 
       // res.send({success:true, master: master_str, includes: inc_str, err : err});             
 }, 6000);
